f96a6a225aed2e860aa6bdc848a20ab28f11c559
Mike <mikewycklendt@gmail.com>
2020-08-29 13:18:16 -0500

    homepage design correct

--- db.py
+++ db.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 moment = Moment(app)
