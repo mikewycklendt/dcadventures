93e3d42b494d5fb5ba75e47d36cb211533eca318
Mike <mikewycklendt@gmail.com>
2020-08-31 12:27:26 -0500

    migrations working

--- app.py
+++ app.py
@@ -33,81 +33,35 @@ def index():
 	meta_content="An online DC Comics Roleplaying game. Play as your favorite character or create your own hero."
 	return render_template('template.html', includehtml=includehtml, title=title, stylesheets=stylesheets, meta_name=meta_name, meta_content=meta_content)
 
-@app.route('/abilities/create')
-def abilities_create():
-	abilities = []
-		
-	abilities.append({'name': "Strength",
-		'description': ['Damage dealt by your unarmed and strength-based attacks.',
-						'How far you can jump.',
-						'The amount of weight you can lift, carry, and throw.',
-						'Athletics skill checks.'],
-		'summary': 'Strength measures sheer muscle power and the ability to apply it. Your Strength rank applies to:'})
+@app.route('/defense/create')
+def defense_create():
 
-	abilities.append({'name': "Stamina",
-		'description': ['Toughness defense, for resisting damage.',
-						'Fortitude defense, for resisting effects targeting your character’s health.',
-						'Stamina checks to resist or recover from things af-fecting your character’s health when a specific de-fense doesn’t apply.'],
-		'summary': 'Stamina is health, endurance, and overall physical resil-ience. Stamina is important because it affects a charac-ter’s ability to resist most forms of damage. Your Stamina modifier applies to:'})
+	defenses = []
 
-	abilities.append({'name': "Agility",
-		'description': ['Dodge defense, for avoiding ranged attacks and oth-er hazards.',
-						'Initiative bonus, for acting first in combat.',
-						'Acrobatics and Stealth skill checks.',
-						'Agility checks for feats of coordination, gross move-ment, and quickness when a specific skill doesn’t apply.'],
-		'summary': 'Agility is balance, grace, speed, and overall physical coor-dination. Your Agility rank applies to:'})
+@app.route('/absent')
+def absent():
 
-	abilities.append({'name': "Dexterity",
-		'description': ['Attack checks for ranged attacks.',
-						'Sleight of Hand and Vehicles skill checks.',
-						'Dexterity checks for feats of fine control and preci-sion when a specific skill doesn’t apply.'],
-		'summary': 'Dexterity is a measure of hand-eye coordination, precision, and manual dexterity. Your Dexterity rank applies to:'})
-					
-	abilities.append({'name': "Fighting",
-		'description': ['Attack checks for close attacks.',
-						'Parry defense, for avoiding close attacks'],
-		'summary': 'Fighting measures your character’s ability in close com-bat, from hitting a target to ducking and weaving around any counter-attacks. Your Fighting rank applies to:'})
+	absent = []
 
-	abilities.append({'name': "Intellect",
-		'description': ['Expertise, Investigation, Technology, and Treatment skill checks.',
-						'Intellect checks to solve problems using sheer brain-power when a specific skill doesn’t apply.'],
-		'summary': 'Intellect covers reasoning ability and learning. A character with a high Intellect rank tends to be knowledgeable and well educated. Your Intellect modifier applies to:'})
+	absent.append({'id': 1, 'value': 'A creature with no Strength is incapable of exerting any physical force, either because it has no physical form (like an incorporeal ghost) or simply can’t move (like a tree).'})
+	absent.append({'id': 2, 'value': 'A creature with no Stamina has no physical body (like a ghost) or is not a living being (such as a robot or other construct). Creatures with no Stamina suffer and recover from damage like inanimate ob-jects (see Damaging Objects under the Damage effect). They are immune to fatigued and exhausted conditions, but cannot exert extra effort. Creatures with no Stamina are often—but not necessarily—immune to many of the other things affecting living beings as well (see the Immunity effect in the Powers chapter). They have no Fortitude defense.'})
+	absent.append({'id': 3, 'value': 'A creature with no Agility is unable to move its body under its own power and has no Dodge defense.'})
+	absent.append({'id': 4, 'value': 'A creature with no Dexterity cannot manipulate objects and hence cannot make physical attacks.'})
+	absent.append({'id': 5, 'value': 'A creature with no Fighting is incapable of making any sort of close attack (but may still be able to launch ranged attacks, if it has Dexterity).'})
+	absent.append({'id': 6, 'value': 'A creature with no Intellect is an automaton, lacking free will and operating entirely on simple instinct or pre-programmed instructions. Anything with no Intellect is immune to mental effects and interaction skills and has no Will defense.'})
+	absent.append({'id': 7, 'value': 'Anything with no Awareness is completely unaware and also has no Presence. It is an inanimate object, not a creature. Objects are immune to mental effects and interaction skills, and have no defenses apart from Toughness (and Fortitude, if they are alive).'})
+	absent.append({'id': 8, 'value': 'Creatures without Presence are unable to interact and are immune to interaction skills. They have no Will defense.'})
 
-	abilities.append({'name': "Awareness",
-		'description': ['Will defense, for resisting attacks on your mind.',
-						'Insight and Perception skill checks.',
-						'Awareness checks to resolve matters of intuition when a specific skill doesn’t apply.'],
-		'summary': 'While Intellect covers reasoning, Awareness describes common sense and intuition, what some might call “wis-dom.” A character with a high Intellect and a low Aware-ness may be an “absent-minded professor” type, smart but not always aware of what’s going on. On the other hand, a not so bright (low Intellect) character may have a great deal of common sense (high Awareness). Your Awareness modifier applies to:'})
-
-	abilities.append({'name': "Presence",
-		'description': ['Deception, Intimidation, and Persuasion skill checks.',
-						'Presence checks to influence others through force of personality when a specific skill doesn’t apply.'],
-		'summary': 'Presence is force of personality, persuasiveness, leader-ship ability and (to a lesser degree) attractiveness. Pres-ence is useful for heroes who intend to be leaders as well as those who strike fear into the hearts of criminals with their presence. Your Presence modifier applies to:'})
-	
-	for ability in abilities:
-		print (ability['name'])
-		name = ability['name']
-		print (ability['summary'])
-		summary = ability['summary']
-		description = ability['description']
-		newEntry = Ability(name=name, description=description, summary=summary)
-		db.session.add(newEntry)
+	for entry in absent:
+		print (entry['id'])
+		print (entry['value'])
+		ability_id = entry['id']
+		value = entry['value']
+		'''
+		ability = Ability.query.filter_by(id=ability_id).one()
+		ability.absent = value
 		db.session.commit()
-		for describe in description:
-			print (describe)
-
-	return ('abilities')
-
-@app.route('/venues/results')
-def venues_results():
-	abilities = Ability.query.all()
-	data = [ability.format() for ability in abilities]
-
-	print (data)
-
-	return 'results'
-
-
+		'''
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=80)
