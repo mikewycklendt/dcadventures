780d3d51ae9a5f0d15736166964891fd09dc848e
Mike <mikewycklendt@gmail.com>
2020-09-03 10:06:57 -0500

    checks added

--- app.py
+++ app.py
@@ -122,66 +122,6 @@ def debilitated_create():
 		})
 '''
 
-@app.route('/check/create')
-def check_create():
-
-	checks = []
-
-	checks.append({
-		'name': 'Team Check',
-		'critical': False,
-		'dc': True,
-		'opposed': True,
-		'automatic': False,
-		'routine': False,
-		'graded': True,
-		'fail': None
-	})
-
-	checks.append({
-		'name': 'Attack Check',
-		'critical': True,
-		'dc': False,
-		'opposed': True,
-		'automatic': False,
-		'routine': False,
-		'graded': False,
-		'fail': 1
-	})
-
-	checks.append({
-		'name': 'Resistance Check',
-		'critical': False,
-		'dc': True,
-		'opposed': False,
-		'automatic': False,
-		'routine': False,
-		'graded': True,
-		'fail': None
-	})
-
-	for check in checks:
-		name = check['name']
-		critical = check['critical']
-		dc = check['dc']
-		opposed = check['opposed']
-		automatic = check['automatic']
-		routine = check['routine']
-		graded = check['graded']
-		fail = check['fail']
-
-		entry = Check(name=name, critical=critical, dc=dc, opposed=opposed, automatic=automatic, routine=routine, graded=graded, fail=fail)
-		db.session.add(entry)
-		db.session.commit()
-
-	results = Check.query.all()
-
-	for result in results:
-		print (result.id)
-		print (result.name)
-
-	return ('Checks Added')
-
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=80)
