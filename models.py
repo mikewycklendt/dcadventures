04b93c664c32b1c61112b08aa0092b570fa15587
Mike <mikewycklendt@gmail.com>
2020-09-05 12:13:07 -0500

    measurements table filled. format functions

--- models.py
+++ models.py
@@ -29,7 +29,6 @@ class Ability(db.Model):
 	name = db.Column(db.String())
 	description = db.Column(db.ARRAY(db.String))
 	summary = db.Column(db.String())
-	absent = db.Column(db.String())
 	modifier_id = db.Column(db.Integer, db.ForeignKey('modifiers.id'))
 
 	def format(self):
@@ -38,7 +37,6 @@ class Ability(db.Model):
 			'name': self.name,
 			'description': self.description,
 			'summary': self.summary,
-			'absent': self.absent,
 			'modifier_id': self.modifier_id
 		}
 
