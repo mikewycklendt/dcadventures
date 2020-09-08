0f4c53e1e23db225944506320e3f9a5bf0dcc7c3
Mike <mikewycklendt@gmail.com>
2020-09-08 07:19:23 -0500

    cleaned template, started create skill page

---
+++ measurements.py
@@ -0,0 +1,32 @@
+from decimal import *
+
+def decRound(value):
+	decimal = Decimal(value).quantize(Decimal('.001'), rounding=ROUND_UP)
+	return decimal
+
+def divide(value1, value2):
+	value = Decimal(value1) / Decimal(value2)
+	decimal = Decimal(value).quantize(Decimal('.001'), rounding=ROUND_UP)
+	return decimal
+
+def multiply(value1, value2):
+	value = Decimal(value1) * Decimal(value2)
+	decimal = Decimal(value).quantize(Decimal('.01'), rounding=ROUND_UP)
+	return decimal
+
+def measure(measurements):
+
+	for measurement in measurements:
+		mass = measurement['mass']
+		time = measurement['time']
+		distance = measurement['distance']
+		volume = measurement['volume']
+
+		measurement['mass'] = Decimal(mass).quantize(Decimal('.01'))
+		measurement['time'] = Decimal(time).quantize(Decimal('.01'))
+		measurement['distance'] = Decimal(distance).quantize(Decimal('.01'))
+		measurement['volume'] = Decimal(volume).quantize(Decimal('.01'))
+
+	print (measurements)
+
+	return measurements
