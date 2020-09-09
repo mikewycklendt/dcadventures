1f2b731f4c4006d2d70ff59c70f0e34bc32affdf
Mike <mikewycklendt@gmail.com>
2020-09-09 09:58:54 -0500

    cleaned template, started create skill page

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def skill_create(stylesheets=stylesheets, meta_name=meta_name, meta_content=meta
 	
 	numbers = []
 	for i in range(-20, 21, 1):
-		numbers += i
+		numbers.append(i)
 
 	return render_template('template.html', actions=actions, conditions=conditions, checks=checks, numbers=numbers, skills=skills, includehtml=includehtml, title=title, stylesheets=stylesheets, meta_name=meta_name, meta_content=meta_content, sidebar=sidebar)
 
