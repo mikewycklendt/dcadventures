ce0fd7a428f8fe0ef9268a35e354d1b56c3ca322
Mike <mikewycklendt@gmail.com>
2020-09-14 13:01:05 -0500

    dc fields complete

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def index(stylesheets=stylesheets, meta_name=meta_name, meta_content=meta_conten
 def skill_create(stylesheets=stylesheets, meta_name=meta_name, meta_content=meta_content, sidebar=sidebar):
 	includehtml = 'special_skill_create.html'
 	
-	skill_includes = {'base_form': 'special_skill_create/base_form.html','dc_table': 'special_skill_create/dc_table.html', 'levels': 'special_skill_create/levels.html', 'circumstance': 'special_skill_create/circumstance_table.html'}
+	skill_includes = {'base_form': 'special_skill_create/base_form.html','dc_table': 'special_skill_create/dc_table.html', 'levels': 'special_skill_create/levels.html', 'circumstance': 'special_skill_create/circumstance_table.html', 'degree': 'special_skill_create/degree_table.html'}
 
 	title = 'DC Adventures Online Roleplqying Game: Create Special Skill'
 	stylesheets.append({"style": "/static/css/special_skill_create.css"})
