55f1bb9dd53034f61148244cb54f0d48477b2fe1
Mike <mikewycklendt@gmail.com>
2020-09-16 10:18:35 -0500

    dc fields complete

--- app.py
+++ app.py
@@ -20,8 +20,6 @@ load_dotenv()
 
 import os
 
-from tables import *
-
 db_path = os.environ.get("db_path")
 
 #db_drop_and_create_all()
@@ -96,6 +94,173 @@ def skill_create(stylesheets=stylesheets, meta_name=meta_name, meta_content=meta
 
 	return render_template('template.html', ranks=ranks, deg_mod_type=deg_mod_type, measure_rank=measure_rank, level_target=level_target, skill_includes=skill_includes, units=units, defenses=defenses, value_type=value_type, maths=maths, dc_rank=dc_rank, dcclasses=dcclasses, dctype=dctype, skilltype=skilltype, actions=actions, conditions=conditions, checks=checks, numbers=numbers, skills=skills, includehtml=includehtml, title=title, stylesheets=stylesheets, meta_name=meta_name, meta_content=meta_content, sidebar=sidebar)
 
+@app.route('/abilities')
+def abilities():
+
+	title = 'Abilities'
+	size = 'h1' 
+	table = Ability.query.all()
+
+	return render_template('table.html', table=table, title=title, size=size)
+
+@app.route('/defense')
+def defense():
+
+	title = 'Defense'
+	size = 'h2'
+	table = Defense.query.all()
+
+	return render_template('table.html', table=table, title=title, size=size)
+
+@app.route('/modifiers')
+def modifiers():
+
+	title = 'Modifiers'
+	
+	size = 'h1'
+
+	table = Modifier.query.all()
+
+	return render_template('table.html', table=table, title=title, size=size)
+
+@app.route('/actions')
+def actions():
+
+	title = 'Actions'
+	
+	size = 'h1'
+
+	table = Action.query.all()
+
+	return render_template('table.html', table=table, title=title, size=size)
+
+@app.route('/skills')
+def skills():
+
+	title = 'Skills'
+	
+	size = 'h1'
+
+	table = Skill.query.all()
+
+	return render_template('table.html', table=table, title=title, size=size)
+
+@app.route('/skill/type')
+def skill_type():
+
+	title = 'Skill Type'
+	
+	size = 'h1'
+
+	table = SkillType.query.all()
+
+	return render_template('table.html', table=table, title=title, size=size)
+
+@app.route('/checks')
+def checks():
+
+	title = 'Check Types'
+	
+	size = 'h1'
+
+	table = Check.query.all()
+
+	return render_template('table.html', table=table, title=title, size=size)
+
+@app.route('/conditions')
+def conditions():
+
+	title = 'Basic Conditions'
+	
+	size = 'h1'
+
+	table = Condition.query.all()
+
+	return render_template('table.html', table=table, title=title, size=size)
+
+@app.route('/phases')
+def phases():
+
+	title = 'Phases'
+	
+	size = 'h1'
+
+	table = Phase.query.all()
+
+	return render_template('table.html', table=table, title=title, size=size)
+
+@app.route('/senses')
+def senses():
+
+	title = 'Senses'
+	
+	size = 'h1'
+
+	table = Sense.query.all()
+
+	return render_template('table.html', table=table, title=title, size=size)
+
+@app.route('/measuretype')
+def measurement_type():
+
+	title = 'Measurement Type'
+	
+	size = 'h1'
+
+	table = MeasureType.query.all()
+
+	return render_template('table.html', table=table, title=title, size=size)
+
+@app.route('/units')
+def unit_type():
+
+	title = 'Measurement Units'
+	
+	size = 'h1'
+
+	table = Unit.query.all()
+
+	return render_template('table.html', table=table, title=title, size=size)
+
+@app.route('/ranks')
+def rank_type():
+
+	title = 'Measurement Units'
+	
+	size = 'h1'
+
+	table = Rank.query.all()
+
+	return render_template('ranks.html', table=table, title=title, size=size)
+
+
+@app.route('/math')
+def math_type():
+
+	title = 'Measurement Units'
+	
+	size = 'h1'
+
+	table = Math.query.all()
+
+	return render_template('table.html', table=table, title=title, size=size)
+
+@app.route('/measurements')
+def measurements():
+
+	title = 'Measurements Table'
+	
+	size = 'h3'
+
+	measurements = Measurement.query.all()
+
+	formatted = [measurement.format() for measurement in measurements]
+	
+	print (formatted)
+
+	table = measure(formatted)
+
+	return render_template('measurements.html', table=table, title=title, size=size)
 
 '''
 @app.route('/debilitated/create')
