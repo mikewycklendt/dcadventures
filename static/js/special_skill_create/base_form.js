2ee7f35375c0646cbd291a12cb26b7fb6ca37d2b
Mike <mikewycklendt@gmail.com>
2020-09-21 09:29:15 -0500

    dc fields complete

---
+++ static/js/special_skill_create/base_form.js
@@ -0,0 +1,10 @@
+function untrained_check() {
+	const untrained_check = document.getElementById("untrained_check");
+	const untrained_details_field = document.getElementById("untrained-details-field");
+	
+	if (untrained_check.checked == true) {
+		untrained_details_field.style.display = "grid"
+	} else {
+		untrained_details_field.xtyle.display = "none"
+	}
+}
