7a39492dfe6906fc92677f4d8621ed9d117d5208
Mike <mikewycklendt@gmail.com>
2020-09-21 11:21:07 -0500

    dc fields complete

--- static/js/special_skill_create/base_form.js
+++ static/js/special_skill_create/base_form.js
@@ -8,3 +8,14 @@ function untrained_check() {
 		untrained_details_field.style.display = "none"
 	}
 }
+
+function subskill_check() {
+	const subskill_check = document.getElementById("subskill_check");
+	const subskill = document.getElementById("subskill");
+	
+	if (subskill_check.checked == true) {
+		subskill.style.display = "grid"
+	} else {
+		subskill.style.display = "none"
+	}
+}
