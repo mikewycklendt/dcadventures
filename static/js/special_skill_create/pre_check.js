9f4f17c7ed7f87f98a8164e4c697dbfd54817296
Mike <mikewycklendt@gmail.com>
2020-09-22 11:21:51 -0500

    skill form complete and hidden

--- static/js/special_skill_create/pre_check.js
+++ static/js/special_skill_create/pre_check.js
@@ -16,13 +16,13 @@ function pre_check_entry() {
 	pre_check_entry_standard = document.getElementById("pre-check-entry-standard")
 	pre_check_entry_opposed = document.getElementById("pre-check-entry-standard")
 
-	if (pre_check_type_value = 'skill') {
+	if (pre_check_type_value == 'skill') {
 		pre_check_entry_standard.style.display = "grid";
 		pre_check_entry_standard.style.padding = "1%";
 		pre_check_entry_standard.style.maxHeight = pre_check_entry_standard.scrollHeight + "px";
 		pre_check_entry_standard.style.padding = "1%";
 		pre_check_entry_opposed.style.display = "none";
-	} else if (pre_check_type_value = 'opposed') {
+	} else if (pre_check_type_value == 'opposed') {
 		pre_check_entry_standard.style.display = "none";
 		pre_check_entry_opposed.style.display = "grid";
 		pre_check_entry_opposed.style.padding = "1%";
