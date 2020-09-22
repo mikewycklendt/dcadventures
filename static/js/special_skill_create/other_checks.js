8bd00e73dada9491006472daeeb9263888ba9475
Mike <mikewycklendt@gmail.com>
2020-09-22 05:26:06 -0500

    skill form complete and hidden

--- static/js/special_skill_create/other_checks.js
+++ static/js/special_skill_create/other_checks.js
@@ -6,7 +6,6 @@ function other_check() {
 		other_entry.style.display = "grid"
 		other_entry.style.maxHeight = other_entry.scrollHeight + "px";
 	} else {
-		other_entry.style.maxHeight = 0;
-		other_entry.style.display = "none"
+		other_entry.style.maxHeight = "0px";
 	}
 }
