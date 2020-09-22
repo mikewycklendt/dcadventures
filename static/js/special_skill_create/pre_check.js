5e07896326806ec2a6df450d412c175434e13f84
Mike <mikewycklendt@gmail.com>
2020-09-22 11:10:47 -0500

    skill form complete and hidden

--- static/js/special_skill_create/pre_check.js
+++ static/js/special_skill_create/pre_check.js
@@ -8,3 +8,28 @@ function pre_check_check() {
 		pre_check_header_type.style.opacity = "0%";
 	}
 }
+
+function pre_check_entry() {
+	pre_check_type = document.getElementById("pre_check_type")
+	pre_check_type_value = pre_check_type.value
+	pre_check_entry_standard = document.getElementById("pre-check-entry-standard")
+	pre_check_entry_opposed = document.getElementById("pre-check-entry-standard")
+
+	if (pre_check_type_value = 'skill') {
+		pre_check_entry_standard.style.display = "grid";
+		pre_check_entry_standard.style.padding = "1%";
+		pre_check_entry_standard.style.maxHeight = pre_check_entry_standard.scrollHeight + "px";
+		pre_check_entry_standard.style.padding = "1%";
+		pre_check_entry_opposed.style.display = "none";
+	} else if (pre_check_type_value = opposed) {
+		pre_check_entry_standard.style.display = "none";
+		pre_check_entry_opposed.style.display = "grid";
+		pre_check_entry_opposed.style.padding = "1%";
+		pre_check_entry_opposed.style.maxHeight = pre_check_entry_opposed.scrollHeight + "px";
+		pre_check_entry_opposed.style.padding = "1%";
+	} else {
+		pre_check_entry_opposed.style.display = "none";
+		pre_check_entry_standard.styl.display = "none";
+
+	}
+}
