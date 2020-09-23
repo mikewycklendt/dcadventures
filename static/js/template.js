c48103d5f2eab73e40caca08b3627e1792ffa3dc
Mike <mikewycklendt@gmail.com>
2020-09-23 08:42:24 -0500

    skill form complete and hidden

--- static/js/template.js
+++ static/js/template.js
@@ -3,7 +3,5 @@ function banner_size() {
 	const banner_img = document.getElementById("the-banner");
 	const banner_width = banner_img.scrollWidth
 	const banner_height = banner_img.scrollHeight
-	const banner_height_less = banner_height - 5;
 	banner_grid.style.width = banner_width + "px";
-	banner_grid.style.height = banner_height_less + "px";
 }
