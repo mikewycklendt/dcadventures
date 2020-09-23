2953bdafeab60ddcb4b118e7ec0b230ffdb1aa47
Mike <mikewycklendt@gmail.com>
2020-09-23 12:01:25 -0500

    skill form complete and hidden

--- static/js/special_skill_create/opposed.js
+++ static/js/special_skill_create/opposed.js
@@ -10,17 +10,16 @@ function opposed_by_check() {
 }
 
 function opposed_by_by() {
-	opposed_by_by = document.getElementById("opposed_by_by")
-	opposed_by_by_value = pre_check_type.options[opposed_by_by.selectedIndex].value;
+	const opposed_by_by = document.getElementById("opposed_by_by")
+	let opposed_by_by_value = pre_check_type.options[opposed_by_by.selectedIndex].value;
 	console.log(opposed_by_by_value)
-	opposed_by_entry = document.getElementById("opposed-by-entry")
+	const opposed_by_entry = document.getElementById("opposed-by-entry")
 
 	if (opposed_by_by_value != '') {
 		opposed_by_entry.style.display = "grid";
 		opposed_by_entry.style.padding = "1%";
 		opposed_by_entry.style.maxHeight = opposed_by_entry.scrollHeight + "px";
 		opposed_by_entry.style.padding = "1%";
-
 	} else {
 		opposed_by_entry.style.maxHeight = "0px";
 		opposed_by_entry.style.padding = "0px";
