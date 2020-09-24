f555f45d195157a9f8709e39de529bc77646995c
Mike <mikewycklendt@gmail.com>
2020-09-24 11:22:57 -0500

    skill form complete and hidden

--- static/js/special_skill_create/circumstance_table.js
+++ static/js/special_skill_create/circumstance_table.js
@@ -8,3 +8,23 @@ function circ_check() {
 		circ_base_form.style.opacity = "0%";
 	}
 }
+
+function circ_base() {
+	const circ_skill = document.getElementById("circ_skill");
+	const circ_target = document.getElementById("circ_target");
+	circskill = circ_skill.options[circ_skill.selectedIndex].value;
+	circtarget =  circ_target.options[circ_target.selectedIndex].value;
+	console.log(circskill);
+	console.log(circtarget);
+	const circ_entry = document.getElementById("circ-entry");
+
+	if (circskill != '' && circtarget != '') {
+		circ_entry.style.display = "grid";
+		circ_entry.style.padding = "1%";
+		circ_entry.style.maxHeight = circ_entry.scrollHeight + "px";
+		circ_entry.style.padding = "1%";
+	} else {
+		circ_entry.style.maxHeight = "0px";
+		circ_entry.style.padding = "0px";
+	}
+}
