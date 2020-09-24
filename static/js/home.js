35f8b74174b0bbcf3dee3eddc1804756c69f2fd1
Mike <mikewycklendt@gmail.com>
2020-09-24 08:28:41 -0500

    skill form complete and hidden

--- static/js/home.js
+++ static/js/home.js
@@ -6,9 +6,15 @@ function home_button_placement() {
 	console.log(sidebar_height);
 	splash.style.height = sidebar_height + "px";
 	blank = document.getElementById("blank")
+	buttons = document.getElementById("buttons")
+	below - document.getElementById("belos")
 	splash_60 = sidebar_height * .6;
+	splash_15 = sidebar_height * .15;
+	splash_25 = sidebar_height * .25;
 	console.log(splash_60);
 	blank.style.height = splash_60 + "px";
+	buttons.style.height = splash_15 + "px";
+	below.style.height = splash_25 + "px";
 }
 
 window.onload = home_button_placement;
