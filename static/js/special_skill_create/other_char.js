98e964983861cadc2ec1d91fae9919ad6cf647af
Mike <mikewycklendt@gmail.com>
2020-09-24 11:45:11 -0500

    skill form complete and hidden

--- static/js/special_skill_create/other_char.js
+++ static/js/special_skill_create/other_char.js
@@ -8,3 +8,23 @@ function char_check() {
 		char_base_form.style.opacity = "0%";
 	}
 }
+
+function char_base() {
+	const char_type = document.getElementById("char_type");
+	const char_target = document.getElementById("char_target");
+	let chartype = circ_skill.options[char_type.selectedIndex].value;
+	let chartarget =  char_target.options[char_target.selectedIndex].value;
+	console.log(chartype);
+	console.log(chartarget);
+	const char_entry = document.getElementById("char-entry");
+
+	if (chartype != '' && chartarget != '') {
+		char_entry.style.display = "grid";
+		char_entry.style.padding = "1%";
+		char_entry.style.maxHeight = char_entry.scrollHeight + "px";
+		char_entry.style.padding = "1%";
+	} else {
+		char_entry.style.maxHeight = "0px";
+		char_entry.style.padding = "0px";
+	}
+}
