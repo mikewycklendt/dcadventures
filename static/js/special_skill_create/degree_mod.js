46f70125dfc1507f505032368d7c02392a0289b4
Mike <mikewycklendt@gmail.com>
2020-09-24 11:35:33 -0500

    skill form complete and hidden

--- static/js/special_skill_create/degree_mod.js
+++ static/js/special_skill_create/degree_mod.js
@@ -8,3 +8,20 @@ function deg_mod_check() {
 		deg_mod_base_form.style.opacity = "0%";
 	}
 }
+
+function deg_mod_base() {
+	const deg_mod_target = document.getElementById("deg_mod_target");
+	degmodtarget =  deg_mod_target.options[deg_mod_target.selectedIndex].value;
+	console.log(degmodtarget);
+	const deg_mod_entry = document.getElementById("deg-mod-entry");
+
+	if (degmodtarget != '') {
+		deg_mod_entry.style.display = "grid";
+		deg_mod_entry.style.padding = "1%";
+		deg_mod_entry.style.maxHeight = deg_mod_entry.scrollHeight + "px";
+		deg_mod_entry.style.padding = "1%";
+	} else {
+		deg_mod_entry.style.maxHeight = "0px";
+		deg_mod_entry.style.padding = "0px";
+	}
+}
