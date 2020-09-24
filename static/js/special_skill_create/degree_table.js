cf01fb45515ab82fdb13f8fef8f23ff0b8937ad0
Mike <mikewycklendt@gmail.com>
2020-09-24 11:28:56 -0500

    skill form complete and hidden

--- static/js/special_skill_create/degree_table.js
+++ static/js/special_skill_create/degree_table.js
@@ -8,3 +8,23 @@ function degree_check() {
 		degree_base_form.style.opacity = "0%";
 	}
 }
+
+function degree_base() {
+	const degree_type = document.getElementById("degree_type");
+	const degree_target = document.getElementById("degree_target");
+	degrees_target =  degree_target.options[degree_target.selectedIndex].value;
+	degrees_type = degree_type.value;
+	console.log(degree_target);
+	console.log(degree_type);
+	const degree_entry = document.getElementById("degree-entry");
+
+	if (degrees_type != '' && degrees_target != '') {
+		degree_entry.style.display = "grid";
+		degree_entry.style.padding = "1%";
+		degree_entry.style.maxHeight = degree_entry.scrollHeight + "px";
+		degree_entry.style.padding = "1%";
+	} else {
+		degree_entry.style.maxHeight = "0px";
+		degree_entry.style.padding = "0px";
+	}
+}
