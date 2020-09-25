ac41f137da4669345ec2b79f75900814c2c564f1
Mike <mikewycklendt@gmail.com>
2020-09-25 09:41:14 -0500

    skill form complete and hidden

--- static/js/special_skill_create/other_checks.js
+++ static/js/special_skill_create/other_checks.js
@@ -12,3 +12,43 @@ function other_check() {
 		other_entry.style.padding = "0px";
 	}
 }
+
+other_enter = 0;
+
+function other_submit() {
+	const table = document.getElementById('other-table');
+
+	table.style.display = "grid";
+	table.style.padding = "1%";
+	table.style.maxHeight = table.scrollHeight + "px";
+	table.style.padding = "1%";
+	
+	let skill_value = document.getElementById('other_skill').value;
+	let examples_value = document.getElementById('other_examples').value; 
+	
+	if (skill_value != '' && examples_value != '') {
+
+		const skill = document.createElement('#other-table-skill');
+		skill.innerHTML = skill_value;
+
+		const examples = document.createElement('#other-table-examples');
+		examples.innerHTML = examples_value;
+	
+		const otherDelete = document.createElement('#other-table-delete');
+		const deleteBtn = document.createElement('button');
+		deleteBtn.className = 'other-xbox';
+		deleteBtn.innerHTML = '&cross;';
+		deleteBtn.setAttribute('data-id', other_enter);
+		otherDelete.appendChild(deleteBtn);
+
+		other_enter = other_enter + 1;
+	
+		table.appendChild(skill);
+		table.appendChild(examples);
+		table.appendChild(otherDelete);
+
+		skill.style.maxHeight = skill.scrollHeight + "px";
+		examples.style.maxHeight = examples.scrollHeight + "px";
+		otherDelete.style.maxHeight = otherDelete.scrollHeight + "px";
+	}
+}
