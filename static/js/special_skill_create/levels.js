0defc149ba7b66c7627fcb344ad1a639be1a052a
Mike <mikewycklendt@gmail.com>
2020-09-26 11:18:05 -0500

    power table

--- static/js/special_skill_create/levels.js
+++ static/js/special_skill_create/levels.js
@@ -31,3 +31,98 @@ function levels_base() {
 		levels_entry.style.padding = "0px";
 	}
 }
+
+
+levels_enter = 0;
+
+function levels_submit() {
+	const table = document.getElementById('levels-table');
+	let trgt_field = document.getElementById('levels_target');
+	let trgt_value = trgt_field.options[trgt_field.selectedIndex].value;
+	let type_value = document.getElementById('level_type').value;
+	let type_field = document.getElementById('level_type');
+
+	const level_type = document.getElementById('levels-table-type');
+
+	level_type.innerHTML = type_value;
+	type_field.style.opacity = "0%;"
+
+	table.style.display = "grid";
+	table.style.padding = "1%";
+	table.style.maxHeight = table.scrollHeight + "px";
+	table.style.padding = "1%";
+	
+	let lvl_value = document.getElementById('level').value;
+	let efct_value = document.getElementById('level_effect').value;
+	let dc_field = document.getElementById('levels_dc_set');
+	let dc_value =  dc_field.options[dc_field.selectedIndex].value; 
+
+	console.log
+	
+	if (type_value != '' && lvl_value != '' && efct_value != '' && dc_value != '' && trgt_value != '') {
+
+		const dc = document.createElement('div');
+		dc.className = 'levels-table-dc'
+		dc.innerHTML = dc_value;
+
+		const lvl = document.createElement('div');
+		lvl.className = 'levels-table-level'
+		lvl.innerHTML = lvl_value;
+
+		const efct = document.createElement('div');
+		efct.className = 'levels-table-effect'
+		efct.innerHTML = efct_value;
+
+		const lvlDelete = document.createElement('div');
+		lvlDelete.className = 'levels-table-delete'
+		const deleteBtn = document.createElement('button');
+		deleteBtn.className = 'levels-xbox';
+		deleteBtn.innerHTML = '&cross;';
+		deleteBtn.setAttribute('data-id', levels_enter);
+		lvlDelete.appendChild(deleteBtn);
+
+		levels_enter = levels_enter + 1;
+	
+		table.appendChild(dc);
+		table.appendChild(lvl);
+		table.appendChild(efct);
+		table.appendChild(lvlDelete);
+
+		
+		dc.style.maxHeight = skill.scrollHeight + "px";
+		lvl.style.maxHeight = skill.scrollHeight + "px";
+		efct.style.maxHeight = skill.scrollHeight + "px";
+		lvlDelete.style.maxHeight = skill.scrollHeight + "px";
+		table.style.maxHeight = table.scrollHeight + 20 + "px";
+
+		levels_delete()
+	}
+};
+
+levels_delete = function() {
+	const deletes = document.querySelectorAll('.levels-xbox');
+	const dcs = document.getElementsByClassName('levels-table-dc');
+	const lvls = document.getElementsByClassName('levels-table-level');
+	const efcts = document.getElementsByClassName('levels-table-effect');
+	const deletesDiv = document.getElementsByClassName('levels-table-delete');
+	for (let i = 0; i < deletes.length; i++) {
+		const btn = deletes[i];
+		btn.onclick = function(e) {
+			console.log('click')
+			dcs[i].style.maxHeight = "0px";
+			dcs[i].style.padding = "0px";
+			dcs[i].style.marginBottom = "0px";
+			lvls[i].style.maxHeight = "0px";
+			lvls[i].style.padding = "0px";
+			lvls[i].style.marginBottom = "0px";
+			efcts[i].style.maxHeight = "0px";
+			efcts[i].style.padding = "0px";
+			efcts[i].style.marginBottom = "0px";
+			deletesDiv[i].style.maxHeight = "0px";
+			deletesDiv[i].style.padding = "0px";
+			deletesDiv[i].style.marginBottom = "0px";
+		}
+	}
+};
+
+levels_delete();
