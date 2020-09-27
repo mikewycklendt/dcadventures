e08eb1c2f0035492057b0c39a7f406faa10e45f1
Mike <mikewycklendt@gmail.com>
2020-09-27 04:26:41 -0500

    levels form avtive

--- static/js/special_skill_create/circumstance_table.js
+++ static/js/special_skill_create/circumstance_table.js
@@ -42,35 +42,36 @@ function circ_mod() {
 
 	if (circ_mod_type_value == 'value') {
 		value_field.style.display = "grid";
+		circ_entry.style.maxHeight = circ_entry.scrollHeight + 20 + "px";
 		value_field.style.padding = "1%";
 		value_field.style.maxHeight = value_field.scrollHeight + "px";
-		circ_entry.style.maxHeight = circ_entry.scrollHeight + "px";
 		math_field.style.display = "none";
 		adjust_field.style.display = "none"
 	} else if (circ_mod_type_value == 'math') {
 		math_field.style.display = "grid";
+		circ_entry.style.maxHeight = circ_entry.scrollHeight + 20 + "px";
 		math_field.style.padding = "1%";
 		math_field.style.maxHeight = math_field.scrollHeight + "px";
-		circ_entry.style.maxHeight = circ_entry.scrollHeight + "px";
 		value_field.style.display = "none";
 		adjust_field.style.display = "none"
 	} else if (circ_mod_type_value == 'Adjust') {
 		adjust_field.style.display = "grid";
+		circ_entry.style.maxHeight = circ_entry.scrollHeight + 20 + "px";
 		adjust_field.style.padding = "1%";
 		adjust_field.style.maxHeight = adj.scrollHeight + "px";
-		circ_entry.style.maxHeight = circ_entry.scrollHeight + "px";
 		value_field.style.display = "none";
 		math_field.style.display = "none"
 	} else if (circ_mod_type_value == 'No equipment') {
 		value_field.style.display = "grid";
+		circ_entry.style.maxHeight = circ_entry.scrollHeight + 20 + "px";
 		value_field.style.padding = "1%";
 		value_field.style.maxHeight = value_field.scrollHeight + "px";
-		circ_entry.style.maxHeight = circ_entry.scrollHeight + "px";
 		adjust_field.style.display = "none"
 	} else {
 		math_field.style.display = "none";
 		value_field.style.display = "none";
 		adjust_field.style.display = "none"
+		circ_entry.style.maxHeight = circ_entry.scrollHeight + "px";
 	}
 
 };
