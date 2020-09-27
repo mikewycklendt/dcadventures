133bf381b12230d6a224ef1d0303f178da7e081b
Mike <mikewycklendt@gmail.com>
2020-09-27 10:07:39 -0500

    degree keyword form active

--- static/js/special_skill_create/degree_mod.js
+++ static/js/special_skill_create/degree_mod.js
@@ -29,6 +29,8 @@ function deg_mod_base() {
 function deg_mod_type() {
 	let deg_mod_type_field = document.getElementById('deg_mod_type');
 	let deg_mod_type_value = deg_mod_type_field.options[deg_mod_type_field.selectedIndex].value;
+
+	console.log(deg_mod_type_value)
 	
 	let damage = document.getElementById('deg-mod-damage');
 	
@@ -53,24 +55,7 @@ function deg_mod_type() {
 		damage.style.display = "grid";
 		damage.style.maxWidth = damage.scrollWidth + "px";
 
-		if(damage_type_value == 'math') {
-			damage_math.style.display = "grid";
-			damage_math.style.maxWidth = damage_math.scrollWidth + "px";
-			damage.style.maxWidth = damage.scrollWidth + damage_math.scrollWidth + "px";
-			damage_value.style.display = "none";
-			damage_value.style.maxWidth = "0px";
-		} else if (damage_type_value == 'value') {
-			damage_value.style.display = "grid";
-			damage_value.style.maxWidth = damage_value.scrollWidth + "px";
-			damage.style.maxWidth = damage.scrollWidth + damage_value.scrollWidth + "px";
-			damage_math.style.display = "none";
-			damage_math.style.maxWidth = "0px";
-		} else {
-			damage_value.style.display = "none";
-			damage_value.style.maxWidth = "0px";
-			damage_math.style.display = "none";
-			damage_math.style.maxWidth = "0px";
-		}
+		
 		measure.style.display = "none";
 		measure.style.maxWidth = "0px";
 		condition.style.display = "none";
@@ -79,31 +64,13 @@ function deg_mod_type() {
 		measure.style.display = "grid";
 		measure.style.maxWidth = measure.scrollWidth + "px";
 
-		if(measure_type_value == 'math') {
-			measure_math.style.display = "grid";
-			measure_math.style.maxWidth = measure_math.scrollWidth + "px";
-			measure.style.maxWidth = measure.scrollWidth + measure_math.scrollWidth + "px";
-			measure_value.style.display = "none";
-			measure_value.style.maxWidth = "0px";
-		} else if (measure_type_value == 'value') {
-			measure_value.style.display = "grid";
-			measure_value.style.maxWidth = measure_value.scrollWidth + "px";
-			measure.style.maxWidth = measure.scrollWidth + measure_value.scrollWidth + "px";
-			measure_math.style.display = "none";
-			measure_math.style.maxWidth = "0px";
-		} else {
-			measure_value.style.display = "none";
-			measure_value.style.maxWidth = "0px";
-			measure_math.style.display = "none";
-			measure_math.style.maxWidth = "0px";
-		}
 		damage.style.display = "none";
 		damage.style.maxWidth = "0px";
 		condition.style.display = "none";
 		condition.style.maxWidth = "0px";
 	} else if (deg_mod_type_value == 'condition') {
 		condition.style.display = "grid";
-		condition.style.maxWidth = measure.scrollWidth + "px";
+		condition.style.maxWidth = condition.scrollWidth + "px";
 		damage.style.display = "none";
 		damage.style.maxWidth = "0px";
 		measure.style.display = "none";
@@ -118,3 +85,61 @@ function deg_mod_type() {
 	}
 
 };
+
+function deg_mod_damage_type() {
+	let damage = document.getElementById('deg-mod-damage');
+	
+	let damage_type_field = document.getElementById('deg_mod_damage_type');
+	let damage_type_value = damage_type_field.options[damage_type_field.selectedIndex].value;
+	
+	let damage_math = document.getElementById('deg-mod-damage-math');
+	let damage_value = document.getElementById('deg-mod-damage-value');
+
+	if(damage_type_value == 'math') {
+		damage_math.style.display = "grid";
+		damage_math.style.maxWidth = damage_math.scrollWidth + "px";
+		damage.style.maxWidth = damage.scrollWidth + damage_math.scrollWidth + "px";
+		damage_value.style.display = "none";
+		damage_value.style.maxWidth = "0px";
+	} else if (damage_type_value == 'value') {
+		damage_value.style.display = "grid";
+		damage_value.style.maxWidth = damage_value.scrollWidth + "px";
+		damage.style.maxWidth = damage.scrollWidth + damage_value.scrollWidth + "px";
+		damage_math.style.display = "none";
+		damage_math.style.maxWidth = "0px";
+	} else {
+		damage_value.style.display = "none";
+		damage_value.style.maxWidth = "0px";
+		damage_math.style.display = "none";
+		damage_math.style.maxWidth = "0px";
+	}
+}
+
+function deg_mod_measure_type() {
+	let measure = document.getElementById('deg-mod-measure');
+
+	let measure_type_field = document.getElementById('deg_mod_measure_type');
+	let measure_type_value = measure_type_field.options[measure_type_field.selectedIndex].value;
+
+	let measure_math = document.getElementById('deg-mod-measure-math');
+	let measure_value = document.getElementById('deg-mod-measure-value');
+	
+	if(measure_type_value == 'math') {
+		measure_math.style.display = "grid";
+		measure_math.style.maxWidth = measure_math.scrollWidth + "px";
+		measure.style.maxWidth = measure.scrollWidth + measure_math.scrollWidth + "px";
+		measure_value.style.display = "none";
+		measure_value.style.maxWidth = "0px";
+	} else if (measure_type_value == 'value') {
+		measure_value.style.display = "grid";
+		measure_value.style.maxWidth = measure_value.scrollWidth + "px";
+		measure.style.maxWidth = measure.scrollWidth + measure_value.scrollWidth + "px";
+		measure_math.style.display = "none";
+		measure_math.style.maxWidth = "0px";
+	} else {
+		measure_value.style.display = "none";
+		measure_value.style.maxWidth = "0px";
+		measure_math.style.display = "none";
+		measure_math.style.maxWidth = "0px";
+	}
+}
