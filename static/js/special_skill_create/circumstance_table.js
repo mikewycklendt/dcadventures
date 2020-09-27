6e3a521045d0f2570bdedc45ab8c4e9fbd971319
Mike <mikewycklendt@gmail.com>
2020-09-27 04:15:33 -0500

    levels form avtive

--- static/js/special_skill_create/circumstance_table.js
+++ static/js/special_skill_create/circumstance_table.js
@@ -34,33 +34,38 @@ function circ_mod() {
 	let circ_mod_type_field = document.getElementById('circ_mod_type');
 	let circ_mod_type_value =  circ_mod_type_field.options[circ_mod_type_field.selectedIndex].value;
 	let mod_field = document.getElementById('circ-mod-field');
-	let value_field = document.getElementById('circ-mod-value');
-	let math_field = document.getElementById('circ-mod-math');
-	let adjust_field = document.getElementById('circ-mod-adjust');
+	const value_field = document.getElementById('circ-mod-value');
+	const math_field = document.getElementById('circ-mod-math');
+	const adjust_field = document.getElementById('circ-mod-adjust');
+	const circ_entry = document.getElementById("circ-entry");
+
 
 	if (circ_mod_type_value = 'value') {
 		value_field.style.display = "grid";
 		value_field.style.padding = "1%";
 		value_field.style.maxHeight = value_field.scrollHeight + "px";
+		circ_entry.style.maxHeight = circ_entry.scrollHeight + "px";
 		math_field.style.display = "none";
 		adjust_field.style.display = "none"
 	} else if (circ_mod_type_value = 'math') {
 		math_field.style.display = "grid";
 		math_field.style.padding = "1%";
 		math_field.style.maxHeight = math_field.scrollHeight + "px";
+		circ_entry.style.maxHeight = circ_entry.scrollHeight + "px";
 		value_field.style.display = "none";
 		adjust_field.style.display = "none"
 	} else if (circ_mod_type_value = 'Adjust') {
 		adjust_field.style.display = "grid";
 		adjust_field.style.padding = "1%";
 		adjust_field.style.maxHeight = adj.scrollHeight + "px";
-		math_field.style.display = "none";
+		circ_entry.style.maxHeight = circ_entry.scrollHeight + "px";
+		value_field.style.display = "none";
 		math_field.style.display = "none"
 	} else if (circ_mod_type_value = 'No equipment') {
 		value_field.style.display = "grid";
 		value_field.style.padding = "1%";
 		value_field.style.maxHeight = value_field.scrollHeight + "px";
-		mod_field.style.display = "grid";
+		circ_entry.style.maxHeight = circ_entry.scrollHeight + "px";
 		adjust_field.style.display = "none"
 	} else {
 		math_field.style.display = "none";
