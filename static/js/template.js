9d09ae350d04e82aa96fb7d7e703fe0c2e827066
Mike <mikewycklendt@gmail.com>
2020-09-29 07:43:05 -0500

    opponent condition form active

--- static/js/template.js
+++ static/js/template.js
@@ -15,14 +15,14 @@ function banner_size() {
 		const splashes = document.getElementsByClassName("splash");
 		const splash = splashes[0];
 		let sidebar_height = sidebargrid.scrollHeight;
-		console.log(sidebar_height);
-		splash.style.height = sidebar_height + "px";
+		let homeimg_height = homeimg.scrollHeight
+		splash.style.height = homeimg_height + "px";
 		blank = document.getElementById("blank");
 		buttons = document.getElementById("buttons");
-		below - document.getElementById("belos");
-		splash_60 = sidebar_height * .6;
-		splash_15 = sidebar_height * .15;
-		splash_25 = sidebar_height * .25;
+		below - document.getElementById("below");
+		splash_60 = homeimg_height * .6;
+		splash_15 = homeimg_height * .15;
+		splash_25 = homeimg_height * .25;
 		console.log(splash_60);
 		blank.style.height = splash_60 + "px";
 		buttons.style.height = splash_15 + "px";
