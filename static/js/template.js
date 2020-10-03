55db408055475d51dc1dcb6786b68affb4622157
Mike <mikewycklendt@gmail.com>
2020-10-03 07:35:42 -0500

    dc table active

--- static/js/template.js
+++ static/js/template.js
@@ -6,32 +6,8 @@ function banner_size() {
 	banner_grid.style.width = banner_width + "px";
 	
 	const splashes = document.getElementsByClassName("splash");
-
-	function home_resize () {		
-			const sidebargrid = document.getElementById("sidebargrid");
-			console.log('sidebargrid: ' + sidebargrid.scrollHeight)
-			const homeimg = document.getElementById("home-img");
-			console.log('home-img: ' + homeimg.scrollHeight)
-			const splashes = document.getElementsByClassName("splash");
-			const splash = splashes[0];
-			let sidebar_height = sidebargrid.scrollHeight;
-			let homeimg_height = homeimg.scrollHeight
-			splash.style.height = homeimg_height + "px";
-			blank = document.getElementById("blank");
-			buttons = document.getElementById("buttons");
-			below - document.getElementById("below");
-			splash_60 = homeimg_height * .70;
-			splash_15 = homeimg_height * .15;
-			splash_25 = homeimg_height * .15;
-			console.log(splash_60);
-			blank.style.height = splash_60 + "px";
-			buttons.style.height = splash_15 + "px";
-			below.style.height = splash_25 + "px";
-
-			homeimg.onresize = home_resize;
-		}
 	
-	if (splashes === null) {
+	if (splashes == null) {
 		console.log('not home')
 	} else {
 		home_resize();
@@ -43,4 +19,28 @@ window.onresize = banner_size;
 
 function btn_select(item) {
 	item.style.background = 'rgba(175, 1, 1 .3)';
+}
+
+function home_resize () {		
+	const sidebargrid = document.getElementById("sidebargrid");
+	console.log('sidebargrid: ' + sidebargrid.scrollHeight)
+	const homeimg = document.getElementById("home-img");
+	console.log('home-img: ' + homeimg.scrollHeight)
+	const splashes = document.getElementsByClassName("splash");
+	const splash = splashes[0];
+	let sidebar_height = sidebargrid.scrollHeight;
+	let homeimg_height = homeimg.scrollHeight
+	splash.style.height = homeimg_height + "px";
+	blank = document.getElementById("blank");
+	buttons = document.getElementById("buttons");
+	below - document.getElementById("below");
+	splash_60 = homeimg_height * .70;
+	splash_15 = homeimg_height * .15;
+	splash_25 = homeimg_height * .15;
+	console.log(splash_60);
+	blank.style.height = splash_60 + "px";
+	buttons.style.height = splash_15 + "px";
+	below.style.height = splash_25 + "px";
+
+	homeimg.onresize = home_resize;
 }
