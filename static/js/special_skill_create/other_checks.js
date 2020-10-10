2fbef587da851a8e16d9d164f45082c05393ffc8
Mike <mikewycklendt@gmail.com>
2020-10-10 09:30:32 -0500

    clean skill page

--- static/js/special_skill_create/other_checks.js
+++ static/js/special_skill_create/other_checks.js
@@ -11,8 +11,8 @@ function other_check() {
 		other_entry.style.maxHeight = other_entry.scrollHeight + "px";
 		other_entry.style.padding = "1%";
 		title.style.color = "#af0101";
-		title.style.fontSize = "210%";
-		setTimeout(function(){title.style.fontSize = "200%"}, 50);
+		title.style.fontSize = "207%";
+		setTimeout(function(){title.style.fontSize = "200%"}, 75);
 	} else {
 		other_entry.style.maxHeight = "0px";
 		other_entry.style.padding = "0px";
