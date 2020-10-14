d80fb9cb478684a348b59630b56c6596bac6c8f5
Mike <mikewycklendt@gmail.com>
2020-10-14 08:36:28 -0500

    add skill code

--- skills.py
+++ skills.py
@@ -206,23 +206,16 @@ def post_other_checks():
 
 	skill = Skill.query.filter_by(id=skill_id)
 
-	try:
-		bonus = SkillOther(bonus_id=bonus_id, skill_id=skill_id, description=description)
-		db.session.add(bonus)
-		db.session.commit()
-		body['success'] = True
-		body['id'] = bonus.id
-		body['bonus_id'] = bonus.bonus_id
-		body['skill'] = skill.name
-		body['description'] = bonus.description
 
-	except:
-		error = True
-		body['success'] = False
-		body['error'] = 'There was an error processing the request'
-		db.session.rollback()
-	
-	finally:
-		db.session.close()
-		print(body)
-		return jsonify(body)
+	bonus = SkillOther(bonus_id=bonus_id, skill_id=skill_id, description=description)
+	db.session.add(bonus)
+	db.session.commit()
+	body['success'] = True
+	body['id'] = bonus.id
+	body['bonus_id'] = bonus.bonus_id
+	body['skill'] = skill.name
+	body['description'] = bonus.description
+
+	db.session.close()
+	print(body)
+	return jsonify(body)
