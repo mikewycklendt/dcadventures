eb3994648748dbce1b2e88c4e4809dd3816a3d7f
Mike <mikewycklendt@gmail.com>
2020-10-24 10:14:47 -0500

    skill post integrated

--- skills.py
+++ skills.py
@@ -434,7 +434,7 @@ def save_skill():
 
 	db.session.close()
 	flash('Skill ' + name + ' Successfully Created')
-	return redirect(url_for('home'))
+	return redirect('/')
 
 @skills.route('/skill/other_checks/create', methods=['POST'])
 def post_bonus_other_checks():
