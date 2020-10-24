604300d287f2b1f2abedd951b2f5dd47861ec7ff
Mike <mikewycklendt@gmail.com>
2020-10-24 10:02:43 -0500

    skill post integrated

--- static/js/special_skill_create/special_skill_create.js
+++ static/js/special_skill_create/special_skill_create.js
@@ -360,6 +360,7 @@ skill_save = function() {
 		.then(jsonResponse => {
 			console.log(jsonResponse)
 			if (jsonResponse.success) {
+				console.log('success')
 			} else {
 				console.log(jsonResponse)
 				const errors = document.getElementById('skill-bonus-err');
