6c937613266c82ee90c40ddc19a66e139ab2ea74
Mike <mikewycklendt@gmail.com>
2020-11-07 11:44:21 -0600

    circ form

--- app.py
+++ app.py
@@ -53,39 +53,8 @@ def home():
 
 	return render_template('template.html', includehtml=includehtml, title=title, stylesheets=stylesheets, meta_name=meta_name, meta_content=meta_content, sidebar=sidebar)
 
-@app.route('/range/create')
-def range_create():
-
-	ranges = []
-
-	ranges.append({'name': 'Short',
-					'distance': 25
-					})
-					
-	ranges.append({'name': 'Medium',
-					'distance': 50
-					})
-
-	ranges.append({'name': 'Long',
-					'distance': 100
-					})
 
 
-	for r in ranges:
-		name = r['name']
-		distance = r['distance']
-
-		entry = Range(name=name, distance=distance)
-		db.session.add(entry)
-		db.session.commit()
-
-	results = Range.query.all()
-
-	for result in results:
-		print (result.id)
-		print (result.name)
-
-	return ('ranges added')
 '''
 @app.route('/debilitated/create')
 def debilitated_create():
