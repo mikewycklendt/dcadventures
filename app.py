bcf5bd3c331e3659fbbfbd4ab3e061078136355c
Mike <mikewycklendt@gmail.com>
2020-11-09 09:37:37 -0600

    environment form

--- app.py
+++ app.py
@@ -53,24 +53,6 @@ def home():
 
 	return render_template('template.html', includehtml=includehtml, title=title, stylesheets=stylesheets, meta_name=meta_name, meta_content=meta_content, sidebar=sidebar)
 
-@app.route('/ground/create')
-def ground_create():
-
-	ground = ['Sand', 'Soil', 'Hard Clay', 'Packed Earth', 'Solid Rock', 'Impervious']
-
-	for i in ground:
-
-		entry = Ground(name=i)
-		db.session.add(entry)
-		db.session.commit()
-
-	results = Ground.query.all()
-
-	for result in results:
-		print (result.id)
-		print (result.name)
-
-	return ('grounds added')
 
 '''
 @app.route('/debilitated/create')
