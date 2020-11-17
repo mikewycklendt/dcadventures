22bd478827cc9e5a3bae370082760d5f5c12c0d3
Mike <mikewycklendt@gmail.com>
2020-11-17 06:56:30 -0600

    descriptor resist forms

--- powers.py
+++ powers.py
@@ -436,26 +436,22 @@ def power_medium_select():
 	
 	medium_subtype = request.get_json()['medium_subtype']
 
-	try:
-		subtype = db.session.query(MediumSubType).filter_by(id=medium_subtype)
-		mediums = db.session.query(Medium).filter_by(medium_subtype=medium_subtype).order_by(Medium.name).all()
+	subtype = db.session.query(MediumSubType).filter_by(id=medium_subtype)
+	mediums = db.session.query(Medium).filter_by(medium_subtype=medium_subtype).order_by(Medium.name).all()
 		
-		all_subtype = 'Any ' + subtype.name
+	all_subtype = 'Any ' + subtype.name
 
-		options = []
+	options = []
 
-		options.append({'id': '', 'name': subtype.name})
-		options.append({'id': 'all', 'name': all_subtype})
-		options.append({'id': 'new', 'name': 'New'})
+	options.append({'id': '', 'name': subtype.name})
+	options.append({'id': 'all', 'name': all_subtype})
+	options.append({'id': 'new', 'name': 'New'})
 
-		for medium in mediums:
-			options.append({'id': medium.id, 'name': medium.name})
+	for medium in mediums:
+		options.append({'id': medium.id, 'name': medium.name})
 
-		body['options'] = options
+	body['options'] = options
 
-	except:
-		body['success'] = False
-		body['options'] = 'no results'
 
 	print(body)
 	return jsonify(body)
