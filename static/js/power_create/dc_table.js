b5949bbba7721372adcb800325942d383caec740
Mike <mikewycklendt@gmail.com>
2020-11-24 08:04:49 -0600

    descriptor resist forms

---
+++ static/js/power_create/dc_table.js
@@ -0,0 +1,36 @@
+function dc_check() {
+	const check = document.getElementById("dc_check");
+	const title = document.getElementById("dc-title");
+	const base = document.getElementById('dc-base');
+	const entry = document.getElementById("dc-entry");
+
+	if (check.checked == true) {
+		base.style.opacity = '100%';
+		title.style.color = "#af0101";
+		title.style.fontSize = "220%";
+		setTimeout(function(){title.style.fontSize = "200%"}, 75);
+	} else {
+		base.style.opacity = '0%'
+		title.style.color = "#245681";
+		entry.style.maxHeight = "0px";
+		entry.style.padding = "0px";
+		setTimeout(function(){entry.style.display = 'none'}, 400);
+	}
+}
+
+function dc_base() {
+	const field = document.getElementById('dc_extra');
+	const value = field.options[field.selectedIndex].value;
+	const entry = document.getElementById("dc-entry");
+
+	if (value != '') {
+		entry.style.display = "grid";
+		entry.style.padding = "1%";
+		entry.style.maxHeight = entry.scrollHeight + "px";
+		entry.style.padding = "1%";
+	} else {
+		entry.style.maxHeight = "0px";
+		entry.style.padding = "0px";
+		setTimeout(function(){entry.style.display = 'none'}, 400);
+	}
+}
