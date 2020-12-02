2d40509e78bf6a15699b93a5c7eef28bc7f53379
Mike <mikewycklendt@gmail.com>
2020-12-02 07:23:34 -0600

    opposed form

--- powers.py
+++ powers.py
@@ -543,30 +543,26 @@ def get_subsense_select():
 	body = {}
 	body['success'] = True
 
-	sense_id = request.get_json()['sense_id']
-	sense_id = int(sense_id)
+	sense_id_str = request.get_json()['sense_id']
+	sense_id = int(sense_id_str)
 
 
-	try:
-		sense = db.session.query(Sense).filter_by(id=sense_id)
-		subsenses = db.session.query(SubSense).filter_by(sense_id=sense_id).order_by(name).all()
+	sense = db.session.query(Sense).filter_by(id=sense_id)
+	subsenses = db.session.query(SubSense).filter_by(sense_id=sense_id).order_by(name).all()
 		
-		any_sense = 'Any ' + sense.name
-		all_sense = 'All ' + sense.name
+	any_sense = 'Any ' + sense.name
+	all_sense = 'All ' + sense.name
 
-		options = []
+	options = []
 
-		options.append({'id': '', 'name': any_sense})
-		options.append({'id': 0, 'name': all_sense})
+	options.append({'id': '', 'name': any_sense})
+	options.append({'id': 0, 'name': all_sense})
 
-		for subsense in subsenses:
-			options.append({'id': subsense.id, 'name': subsense.name})
+	for subsense in subsenses:
+		options.append({'id': subsense.id, 'name': subsense.name})
 
-		body['options'] = options
+	body['options'] = options
 
-	except:
-		body['success'] = False
-		body['options'] = 'no results'
 
 	print(body)
 	return jsonify(body)
