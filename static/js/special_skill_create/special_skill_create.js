bf3a3cf73ff91f8194ba69d1e49d227509e6798d
Mike <mikewycklendt@gmail.com>
2020-12-05 06:51:14 -0600

    skill create functions

--- static/js/special_skill_create/special_skill_create.js
+++ static/js/special_skill_create/special_skill_create.js
@@ -459,7 +459,8 @@ skill_create = function() {
 			setTimeout(function(){name_div.style.fontSize = "400%"}, 75);
 			edit_button.style.display = "block";
 			add_skill.style.display = "none";
-			all_fields.style.opacity = "100%";
+			all_fields.style.display = "grid";
+			setTimeout(function(){all_fields.style.opacity = "100%"}, 10);
 
 			errors_delete = document.getElementsByClassName('name-err-line');
 
