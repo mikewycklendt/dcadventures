37c10b9df85a2739deef1bf13b807281875fa26a
Mike <mikewycklendt@gmail.com>
2020-12-18 14:23:02 -0600

    js fix

--- power_posts.py
+++ power_posts.py
@@ -342,10 +342,11 @@ def mod_cell(title, width, data, mod, value=None, variable_data=['e']):
 			d = {}
 			d['value'] = value
 			d['cells'] = []
+			
 			d['grid'] = ''
 		else:
 			for var in variable_data:
-				print(var)
+			
 				print('\n\n\n\n')
 				if var['value'] == value:
 					d = var
