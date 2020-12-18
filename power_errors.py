ccf4cf208fd58084d0e17d54c75c41d345ed0cf2
Mike <mikewycklendt@gmail.com>
2020-12-18 08:26:51 -0600

    js fix

--- power_errors.py
+++ power_errors.py
@@ -594,7 +594,7 @@ def defense_post_errors(data):
 
 	errors = variable(reflect_check, data, errors)
 
-	errors = check_fields(immunity, [immunity_type], errors)
+	errors = check_fields(immunity, 'immunity', [immunity_type], errors)
 
 	fields = field('Trait Type', immunity_trait_type)
 	fields = field('Trait', immunity_trait, fields)
