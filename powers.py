4a0fbd70bef83f792d1464d240b935f3273ae9d8
Mike <mikewycklendt@gmail.com>
2020-12-20 07:40:41 -0600

    new python js

--- powers.py
+++ powers.py
@@ -1978,7 +1978,6 @@ def power_post_defense():
 							cover_type = cover_type)
 	db.session.add(entry)
 	db.session.commit()
-	db.session.close()
 
 	body = defense_post(entry, columns, created)
 	return jsonify(body)
