0998e2b84f024e2b1d7a45eefefe5c008027b195
Mike <mikewycklendt@gmail.com>
2020-12-21 14:08:37 -0600

    js table functions

--- static/js/power_create/power_create.js
+++ static/js/power_create/power_create.js
@@ -1035,7 +1035,7 @@ function create_table(jsonResponse) {
 
 		const table = document.getElementById(table_class)
 
-
+		
 		grid__update(cells, table_id, grid, cells_class)
 
 		grow_table(table, grow)
@@ -1091,11 +1091,11 @@ function create_titles(jsonResponse, grow) {
 		}
 		title_row.appendChild(cell_title);
 	}
-
+	```
 	grow += title_row.scrollHeight
 
 	grow_table(new_table, grow)
-	
+	```
 	grid__update(cells, table_id, grid, cells_class)
 }
 
@@ -1171,10 +1171,14 @@ function cells_create(table, grow, jsonResponse) {
 				check.className = base_button_check;
 				title_row.classList.add(check_button_class);
 				new_cell.appendChild(check);
+				const cell_height = new_cell.scrollHeight;
+				cell_heights.push(cell_height);
 			} else {
 				const check = document.createElement('div');
 				check.className = base_check;
-				new_cell.appendChild(check)
+				new_cell.appendChild(check);
+				const cell_height = new_cell.scrollHeight;
+				cell_heights.push(cell_height);
 			}``
 		} else {
 			new_cell.innerText = cell.content;
