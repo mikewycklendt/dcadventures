c05ab5c52a0dd9fe8b8a8a53d414955b1bbfc481
Mike <mikewycklendt@gmail.com>
2020-12-24 07:15:05 -0600

    create cells

--- static/js/power_create/power_create.js
+++ static/js/power_create/power_create.js
@@ -1358,7 +1358,7 @@ function row_delete(jsondata, route, object) {
 
 	const cells_class = table_id + '-cells';
 	const table_class = table_id + '-table'
-	const entry_class = 'power-table-row';
+	const entry_class = table_id + '-row';
 	const table = document.getElementById(table_class);
 	const delete_class = table_id + '-xbox';
 
