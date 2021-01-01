dcab944d821589d2008ba6fa0cbfc82599264cd8
Mike <mikewycklendt@gmail.com>
2021-01-01 09:13:19 -0600

    all table functions

--- powers.py
+++ powers.py
@@ -3892,7 +3892,9 @@ def power_post_sense():
 	ranks = integer(ranks)
 	cost = integer(cost)
 
-	entry = PowerSense(target = target,
+	entry = PowerSense(power_id = power_id,
+						extra_id = extra_id,
+						target = target,
 						sense = sense,
 						subsense = subsense,
 						sense_cost = sense_cost,
