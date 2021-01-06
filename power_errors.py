49ac22e2384f22195c81a0e3d08ecec139fc524e
Mike <mikewycklendt@gmail.com>
2021-01-06 09:05:11 -0600

    power save errors

--- power_errors.py
+++ power_errors.py
@@ -204,12 +204,14 @@ def cost_check(check, name, field, table, power, errors):
 			for c in cost_check:
 				if c.cost is not None:
 					if c.cost != field:
+						error = True
 						message = 'You set a rule for a ' + name + ' effect thwt has a different cost than the main power effect.  Delete that rule or change the main power cost to X.'
 						error_msgs.append(message)
 		else:
 			cost_check = db.session.query(table).filter_by(power_id=power, extra_id=None).all()
 			for c in cost_check:
 				if c.cost is None:
+					error = True
 					message = 'You set a variable cost for this power, so you must delete and recreate the ' + name + ' rule and specify the cost or set a cost for the base power.'
 					error_msgs.append(message)
 				
