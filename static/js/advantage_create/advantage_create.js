5f246b34794515007142c5414e6982fc34e54a41
Mike <mikewycklendt@gmail.com>
2021-01-07 14:22:50 -0600

    advantage start

--- static/js/advantage_create/advantage_create.js
+++ static/js/advantage_create/advantage_create.js
@@ -404,15 +404,11 @@ function double_select_maxheight_entry(select1_input, select2_input, option1, op
 	const field1 = document.getElementById(select1_input);
 	const val1 = field1.options[field1.selectedIndex].value;
 	const field2 = document.getElementById(select2_input);
-	const val2 = field1.options[field1.selectedIndex].value;
+	const val2 = field2.options[field2.selectedIndex].value;
 
-	if (val2 == option2) {
-		console.log('already grown');
-	} else {
-		if (val1 == option1) {
-			show_maxheight(div);
-			grow_entry(entry, div);
-		}
+	if (val2 != option2 && val1 == option1) {
+		show_maxheight(div);
+		grow_entry(entry, div);
 	}
 	
 	if (val1 != option1 && val2 != option2) {
