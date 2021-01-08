f72e2d24a7e9a23dc69eec24562095a088738470
Mike <mikewycklendt@gmail.com>
2021-01-08 13:09:43 -0600

    advantage start

--- static/js/advantage_create/modifiers.js
+++ static/js/advantage_create/modifiers.js
@@ -16,8 +16,9 @@ function modifiers_bonus_effect() {
 	const options = [{'val': 'trait', 'div':'modifiers-row3'}, {'val': 'check', 'div':'modifiers-row3'} ];
 	const entry = 'modifiers-entry';
 	const div = 'modifiers-row3';
-	double_select_maxheight_entry(select1, option1, option2, div, entry)
-	double_select_maxheight_entry(select2, option1, option2, div, entry)
+
+	double_select_maxheight_entry(select1, select2, option1, div, entry)
+	double_select_maxheight_entry(select1, select2, option2, div, entry)
 	
 	select_opacity(select1, divs)
 }
@@ -34,9 +35,8 @@ function modifiers_penalty_effect() {
 	const entry = 'modifiers-entry';
 	const div = 'modifiers-row3';
 	
-	
-	double_select_maxheight_entry(select1, option1, option2, div, entry)
-	double_select_maxheight_entry(select2, option1, option2, div, entry)
+	double_select_maxheight_entry(select1, select2, option1, div, entry)
+	double_select_maxheight_entry(select1, select2, option2, div, entry)
 	
 	select_opacity(select1, divs)
 }
