2103dcb7ec26bf3993ef5eb5ed5d4898057bea65
Mike <mikewycklendt@gmail.com>
2021-01-08 12:24:26 -0600

    advantage start

--- static/js/advantage_create/advantage_create.js
+++ static/js/advantage_create/advantage_create.js
@@ -455,18 +455,23 @@ function select_maxheight_entry(select, options, entry) {
 }
 
 
-function double_select_maxheight_entry(select1_input, select2_input, option, div, entry) {
+function double_select_maxheight_entry(select1_input, select2_input, option1, option2, div, entry) {
 	const field1 = document.getElementById(select1_input);
 	const val1 = field1.options[field1.selectedIndex].value;
 	const field2 = document.getElementById(select2_input);
 	const val2 = field2.options[field2.selectedIndex].value;
 1
-	if (val1 == option || val2 == option)  {
+	if (val1 == option1 || val2 == option1)  {
+		show_maxheight(div);
+		grow_entry(entry, div);
+	}
+
+	if (val1 == option2 || val2 == option2) {
 		show_maxheight(div);
 		grow_entry(entry, div);
 	}
 	
-	if (val1 != option && val2 != option) {
+	if (val1 != option1 && val2 != option1 && val1 != option2 && val2 != option2) {
 		shrink_entry(entry, div);
 		hide_maxheight(div);
 	}
