733f348e886e907fba4c9946a12e109f831bea47
Mike <mikewycklendt@gmail.com>
2021-01-16 11:34:55 -0600

    ADVANTAGE posts complete

--- static/js/advantage_create/benefit.js
+++ static/js/advantage_create/benefit.js
@@ -49,7 +49,7 @@ function benefit_submit() {
 			const id = jsonResponse.id;
 			const benefit_name = jsonResponse.name;
 					
-			let benefits_selects = document.getElementsByClassName('benefit-entry');
+			let benefits_selects = document.getElementsByClassName('benefit-sml');
 			let benefit;
 					
 			for (benefit of benefits_selects) {
@@ -62,7 +62,7 @@ function benefit_submit() {
 								
 			benefits_selects = document.getElementsByClassName('benefit-entry');
 					
-			for (benefit in benefits_selects) {
+			for (benefit of benefits_selects) {
 				const o = document.createElement('option');
 				o.value = id;
 				o.text = benefit_name;
