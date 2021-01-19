b7e6416d0171382bcea25888d6449f7b0317ac4f
Mike <mikewycklendt@gmail.com>
2021-01-19 14:35:27 -0600

    ADVANTAGE save

--- base_files.py
+++ base_files.py
@@ -2,5 +2,5 @@
 stylesheets = [{"style": "/static/css/template.css"}, {"style": "/static/css/sidebar.css"}, {"style": "/static/css/font-awesome.min.css"}]
 meta_name="DC Adventures Online"
 meta_content="An online DC Comics Roleplaying game. Play as your favorite character or create your own hero."
-title = 'DC Adventures Online Roleplqying Game'
+title = 'DC Adventures Online Roleplaying Game'
 sidebar = ["rules", "games", "stories", "heroes","npcs", "locations", "skills", "abilities", "powers", "flaws", "equipment", "devices", "armor", "weapons", "vehicles", "constructs", "help"]
