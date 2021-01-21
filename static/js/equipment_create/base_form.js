b3807c95e9c88a804f33c3b174cd02df365fe46c
Mike <mikewycklendt@gmail.com>
2021-01-21 11:02:12 -0600

    ADVANTAGE save

--- static/js/equipment_create/base_form.js
+++ static/js/equipment_create/base_form.js
@@ -1,5 +1,10 @@
-function equip_range() {
-	const div = 'base-extend';
+function effect_check() {
+	const check = "effect_check";
+	const alt = 'alternate';
+	const title = "effect-title";
+	const base = 'effect-base';
+	const entry = "effect-entry";
 
-	show_opacity(div)
+	entry_check(alt, title, base, entry);
+	check.checked = true;
 }
