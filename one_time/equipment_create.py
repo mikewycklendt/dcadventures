6f77ce3234763f398600483bc4af805c11073943
Mike <mikewycklendt@gmail.com>
2021-01-22 08:53:35 -0600

    ADVANTAGE save

--- one_time/equipment_create.py
+++ one_time/equipment_create.py
@@ -2,13 +2,22 @@
 @app.route('/equiptype/create')
 def equiptype_create():
 
-	entries = ['General Equipment', 'Electronics', 'Criminal Gear', 'Surveillance Gear', 'Survival Gear', 'Utility Belt']
+	entries = ['Camera', 'Cell Phone', 'Commlink', 'Computer', 'Audio Recorder', 'Video Camera']
 
 	for i in entries:
 
-		entry = EquipType(name=i)
+		entry = Equipment(name=i, type_id=1, cost=1)
 		db.session.add(entry)
 		db.session.commit()
+
+	entries = ['Handcuffs', 'Lock Release Gun', 'Resistable']
+
+	for i in entries:
+
+		entry = Equipment(name=i, type_id=2, cost=2)
+		db.session.add(entry)
+		db.session.commit()
+
 
 	results = EquipType.query.all()
 
