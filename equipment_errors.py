bba4a8c4f7e06c99d3d6b2abea5d2e2579f2f3c7
Mike <mikewycklendt@gmail.com>
2021-01-24 13:13:58 -0600

    equipment

--- equipment_errors.py
+++ equipment_errors.py
@@ -6,7 +6,7 @@ from models import Equipment, Light, EquipType, Feature, WeaponCat, Weapon, Equi
 
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
-from error_functions import integer, required, power_check, one, field, rule_check, rule_select, cost_check, extra_cost, variable, select, variable_fields, variable_field, select_variable, together, check_together_var, together_names, check_fields, check_field, multiple, check_of_multiple, of_multiple, check_of, of, select_of, id_check, extra_check, extra_convert, int_check, db_integer, db_check, if_fields, if_field, create_check, db_insert, adv_entry_check, adv_check_multiple, adv_check_multiple_fields, adv_select_entry
+from error_functions import integer, required, power_check, one, field, rule_check, rule_select, cost_check, extra_cost, variable, select, variable_fields, variable_field, select_variable, together, check_together_var, together_names, check_fields, check_field, multiple, check_of_multiple, of_multiple, check_of, of, select_of, id_check, extra_check, extra_convert, int_check, db_integer, db_check, if_fields, if_field, create_check, db_insert, adv_entry_check, adv_check_multiple, adv_check_multiple_fields, adv_select_entry, name_exist
 
 
 def equip_belt_post_errors(data):
@@ -142,6 +142,7 @@ def equip_feature_post_errors(data):
 
 	errors = required(name, 'Name', errors)
 	description = required(description, 'Description', errors)
+	errors = name_exist('Feature', Feature, name, errors)
 
 	return (errors)
 
