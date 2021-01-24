dc1a0a2167e8125f6ae2fba56ec345fccfe9138c
Mike <mikewycklendt@gmail.com>
2021-01-24 12:02:19 -0600

    equipment

--- equipment_errors.py
+++ equipment_errors.py
@@ -11,6 +11,8 @@ from error_functions import integer, required, power_check, one, field, rule_che
 
 def equip_belt_post_errors(data):
 
+	errors = {'error': False, 'error_msgs': []}
+
 	equip_id = data['equip_id']
 	columns = data['columns']
 	created = data['created']
@@ -29,6 +31,8 @@ def equip_belt_post_errors(data):
 	return (errors)
 
 def equip_check_post_errors(data):
+
+	errors = {'error': False, 'error_msgs': []}
 
 	equip_id = data['equip_id']
 	columns = data['columns']
@@ -54,6 +58,8 @@ def equip_check_post_errors(data):
 	return (errors)
 
 def equip_damaged_post_errors(data):
+
+	errors = {'error': False, 'error_msgs': []}
 
 	equip_id = data['equip_id']
 	columns = data['columns']
@@ -82,6 +88,7 @@ def equip_damaged_post_errors(data):
 
 def equip_descriptor_post_errors(data):
 
+	errors = {'error': False, 'error_msgs': []}
 
 	equip_id = data['equip_id']
 	columns = data['columns']
@@ -102,20 +109,31 @@ def equip_descriptor_post_errors(data):
 
 def equip_effect_post_errors(data):	
 
+	errors = {'error': False, 'error_msgs': []}
+
 	equip_id = data['equip_id']
 	name = data['name']
 	description = data['description']
 
+	equip_id = id_check(Equipment, equip_id, 'Equipment', errors)
 
-
+	errors = required(name, 'Name', errors)
+	description = required(description, 'Description', errors)
+	
 	return (errors)
 
 def equip_feature_post_errors(data):
 
+	errors = {'error': False, 'error_msgs': []}
+
 	equip_id = data['equip_id']
 	name = data['name']
 	description = data['description']
 
+	equip_id = id_check(Equipment, equip_id, 'Equipment', errors)
+
+	errors = required(name, 'Name', errors)
+	description = required(description, 'Description', errors)
 
 	return (errors)
 
@@ -174,12 +192,143 @@ def equip_limits_post_errors(data):
 
 def equip_modifiers_post_errors(data):
 
+	errors = {'error': False, 'error_msgs': []}
+
+	advantage_id = data['equip_id']
+	columns = data['columns']
+	created = data['created']
+	font = data['font']
+	bonus = data['bonus']
+	bonus_type = data['bonus_type']
+	penalty = data['penalty']
+	penalty_type = data['penalty_type']
+	trigger = data['trigger']
+	bonus_effect = data['bonus_effect']
+	penalty_effect = data['penalty_effect']
+	environment = data['environment']
+	environment_other = data['environment_other']
+	sense = data['sense']
+	mod_range = data['mod_range']
+	subsense = data['subsense']
+	cover = data['cover']
+	conceal = data['conceal']
+	maneuver = data['maneuver']
+	weapon_melee = data['weapon_melee']
+	weapon_ranged = data['weapon_ranged']
+	tools = data['tools']
+	condition = data['condition']
+	power = data['power']
+	consequence = data['consequence']
+	creature = data['creature']
+	creature_other = data['creature_other']
+	emotion = data['emotion']
+	emotion_other = data['emotion_other']
+	conflict = data['conflict']
+	profession = data['profession']
+	profession_other = data['profession_other']
+	bonus_trait_type = data['bonus_trait_type']
+	bonus_trait = data['bonus_trait']
+	bonus_check = data['bonus_check']
+	bonus_check_range = data['bonus_check_range']
+	bonus_conflict = data['bonus_conflict']
+	penalty_trait_type = data['penalty_trait_type']
+	penalty_trait = data['penalty_trait']
+	penalty_check = data['penalty_check']
+	penalty_check_range = data['penalty_check_range']
+	penalty_conflict = data['penalty_conflict']
+	bonus_active_defense = data['bonus_active_defense']
+	bonus_conflict_defend = data['bonus_conflict_defend']
+	penalty_active_defense = data['penalty_active_defense']
+	penalty_conflict_defend = data['penalty_conflict_defend']
+	multiple = data['multiple']
+	multiple_count = data['multiple_count']
+	lasts = data['lasts']
+	skill = data['skill']
+	light = data['light']
+	effect = data['effect']
+	feature = data['feature']
+
+	errors = create_check('Advantage', advantage_id, Advantage, errors)
+	
+	errors = int_check(bonus, 'Bonus', errors)
+	errors = int_check(penalty, 'Penalty', errors)
+	errors = int_check(multiple_count, 'Multiple Count', errors)
+	errors = int_check(lasts, 'Lasts', errors)
+
+	errors = db_insert('Environment', Environment, environment, environment_other, errors)
+	errors = db_insert('Emotion', Emotion, emotion, emotion_other, errors)
+	errors = db_insert('Creature', Creature, creature, creature_other, errors)
+	errors = db_insert('Profession', Job, profession, profession_other, errors)
+
+	errors = db_check(Feature, feature, 'Feature', errors)
+	errors = db_check(EquipEffect, effect, 'Alternate Effect', errors)
+	errors = db_check(Skill, skill, 'Skill', errors)
+	errors = db_check(Light, light, 'Light', errors)
+	errors = db_check(Environment, environment, 'Environment', errors)
+	errors = db_check(Sense, sense, 'Sense', errors)
+	errors = db_check(Ranged, mod_range, 'Range', errors)
+	errors = db_check(SubSense, subsense, 'Subsense', errors)
+	errors = db_check(Cover, cover, 'Cover', errors)
+	errors = db_check(Conceal, conceal, 'Concealment', errors)
+	errors = db_check(Maneuver, maneuver, 'Maneuver', errors)
+	errors = db_check(WeaponType, weapon_melee, 'Melee Weapon Type', errors)
+	errors = db_check(WeaponType, weapon_ranged, 'Ranged Weapon Type', errors)
+	errors = db_check(Consequence, consequence, 'Consequence', errors)
+	errors = db_check(Creature, creature, 'Creature', errors)
+	errors = db_check(Emotion, emotion, 'Emotion', errors)
+	errors = db_check(ConflictAction, conflict, 'Conflict Action', errors)
+	errors = db_check(Job, profession, 'Profession', errors)
+	errors = db_check(Check, bonus_check, 'Bonus Check Type', errors)
+	errors = db_check(Ranged, bonus_check_range, 'Bonus Check Range', errors)
+	errors = db_check(ConflictAction, bonus_conflict, 'Bonus Conflict Action', errors)
+	errors = db_check(Check, penalty_check, 'Penalty Check Type', errors)
+	errors = db_check(Ranged, penalty_check_range, 'Penalty Check Range', errors)
+	errors = db_check(ConflictAction, penalty_conflict, 'Penalty Conflict Action', errors)
+
+	errors = of([bonus, penalty], 'You must set a value for either a bonus or a penalty.', errors)
+
+	errors = if_fields('Bonus', bonus, [bonus_type, bonus_effect], errors)
+	errors = if_field('Bonus', bonus, bonus_type, 'Bonus Type', errors)
+	errors = if_field('Bonus', bonus, bonus_effect, 'Bonus Effect', errors)
+	errors = if_fields('Penalty', penalty, [penalty_type, penalty_effect], errors)
+	errors = if_field('Penalty', penalty, penalty_type, 'Penalty Type', errors)
+	errors = if_field('Penalty', penalty, penalty_effect, 'Penalty Effect', errors)
+
+	errors = variable_fields('trait', 'Bonus Trait', bonus_effect, [bonus_trait_type, bonus_trait], errors)
+	errors = variable_fields('check', 'Bonus Check', bonus_effect, [bonus_check], errors)
+	errors = variable_fields('conflict', 'Bonus Conflict Action', bonus_effect, [bonus_conflict], errors)
+
+	errors = variable_fields('trait', 'Penalty Trait', penalty_effect, [penalty_trait_type, penalty_trait], errors)
+	errors = variable_fields('check', 'Penalty Check', penalty_effect, [penalty_check], errors)
+	errors = variable_fields('conflict', 'Penalty Conflict Action', penalty_effect, [penalty_conflict], errors)
+
+	errors = variable_fields('environment', 'Environment', trigger, [environment], errors)
+	errors = variable_fields('cover', 'Cover', trigger, [cover], errors)
+	errors = variable_fields('conceal', 'Concealment', trigger, [conceal], errors)
+	errors = variable_fields('sense', 'Sense', trigger, [sense], errors)
+	errors = variable_fields('subsense', 'Subsense', trigger, [subsense], errors)
+	errors = variable_fields('condition', 'Condition', trigger, [condition], errors)
+	errors = variable_fields('profession', 'Characters Profession', trigger, [profession], errors)
+	errors = variable_fields('creature', 'Creature', trigger, [creature], errors)
+	errors = variable_fields('power', 'Power', trigger, [power], errors)
+	errors = variable_fields('emotion', 'Emotion', trigger, [emotion], errors)
+	errors = variable_fields('consequence', 'Consequence', trigger, [consequence], errors)
+	errors = variable_fields('range', 'Range', trigger, [mod_range], errors)
+	errors = variable_fields('conflict', 'Conflict Action', trigger, [conflict], errors)
+	errors = variable_fields('maneuver', 'Maneuver', trigger, [maneuver], errors)
+	errors = variable_fields('tools', 'Tool Requirement', trigger, [tools], errors)
+	errors = variable_fields('ranged', 'Ranged Weapon', trigger, [weapon_ranged], errors)
+	errors = variable_fields('melee', 'Melee Weapon', trigger, [weapon_melee], errors)
+	errors = variable_fields('skill', 'Skill', trigger, [skill], errors)
+	errors = variable_fields('light', 'Light', trigger, [light], errors)
+
 
 
 	return (errors)
 
 def equip_opposed_post_errors(data):
 
+	errors = {'error': False, 'error_msgs': []}
 
 	equip_id = data['equip_id']
 	effect = data['effect']
--- equipment_posts.py
+++ equipment_posts.py
@@ -123,8 +123,8 @@ def equip_effect_post(entry, body, cells):
 	name = entry.name
 	description = entry.description
 
-
-
+	cells = cell('Name', 20, [name])
+	cells = cell('Description', 80, [description], cells)
 
 	body = send(cells, body)
 	
@@ -138,6 +138,8 @@ def equip_feature_post(entry, body, cells):
 	name = entry.name
 	description = entry.description
 
+	cells = cell('Name', 20, [name])
+	cells = cell('Description', 80, [description], cells)
 
 	body = send(cells, body)
 	
@@ -198,6 +200,164 @@ def equip_limits_post(entry, body, cells):
 
 def equip_modifiers_post(entry, body, cells):
 
+	equip_id = entry.equip_id
+	feature = entry.feature
+	effect = entry.effect
+	benefit = entry.benefit
+	bonus = entry.bonus
+	bonus_type = entry.bonus_type
+	penalty = entry.penalty
+	penalty_type = entry.penalty_type
+	trigger = entry.trigger
+	bonus_effect = entry.bonus_effect
+	penalty_effect = entry.penalty_effect
+	environment = entry.environment
+	environment_other = entry.environment_other
+	sense = entry.sense
+	mod_range = entry.mod_range
+	subsense = entry.subsense
+	cover = entry.cover
+	conceal = entry.conceal
+	maneuver = entry.maneuver
+	weapon_melee = entry.weapon_melee
+	weapon_ranged = entry.weapon_ranged
+	tools = entry.tools
+	condition = entry.condition
+	power = entry.power
+	consequence = entry.consequence
+	creature = entry.creature
+	creature_other = entry.creature_other
+	emotion = entry.emotion
+	emotion_other = entry.emotion_other
+	conflict = entry.conflict
+	profession = entry.profession
+	profession_other = entry.profession_other
+	bonus_trait_type = entry.bonus_trait_type
+	bonus_trait = entry.bonus_trait
+	bonus_trait = variable_trait(bonus_trait, bonus_trait_type)
+	bonus_check = entry.bonus_check
+	bonus_check_range = entry.bonus_check_range
+	bonus_conflict = entry.bonus_conflict
+	penalty_trait_type = entry.penalty_trait_type
+	penalty_trait = entry.penalty_trait
+	penalty_trait = variable_trait(penalty_trait, penalty_trait_type)
+	penalty_check = entry.penalty_check
+	penalty_check_range = entry.penalty_check_range
+	penalty_conflict = entry.penalty_conflict
+	bonus_active_defense = entry.bonus_active_defense
+	bonus_conflict_defend = entry.bonus_conflict_defend
+	penalty_active_defense = entry.penalty_active_defense
+	penalty_conflict_defend = entry.penalty_conflict_defend
+	multiple = entry.multiple
+	multiple_count = entry.multiple_count
+	lasts = entry.lasts~
+
+	bonus = integer_convert(bonus)
+	penalty = integer_convert(penalty)
+	multiple_count = integer_convert(multiple_count)
+	lasts = integer_convert(lasts)
+
+	modifier_type = [{'type': '', 'name': 'Type'}, {'type': 'up', 'name': 'Up to'}, {'type': 'value', 'name': 'Exact'}, {'type': 'rank', 'name': 'Per Rank'},  {'type': '-1', 'name': 'Rank - 1'}]
+	bonus_type = selects(bonus_type, modifier_type)
+	penalty_type = selects(penalty_type, modifier_type)
+
+	multiple_select = [{'type': '', 'name': 'If Multiple'}, {'type': 'together', 'name': 'All Work Together'}, {'type': 'round', 'name': 'Choose for Round'}, {'type': 'turn', 'name': 'Choose for Turn'}, {'type': 'pick', 'name': 'Pick 1'}, {'type': 'rank', 'name': '1 Per Rank'}]
+	multiple = selects(multiple, multiple_select)
+
+	benefit = name(Benefit, benefit)
+	environment = name(Environment, environment, 'Variable Environment')
+	sense = name(Sense, sense, 'Variable ')
+	mod_range = name(Ranged, mod_range, 'Variable ')
+	subsense = name(SubSense, subsense, 'Variable Subssense')
+	cover = name(Cover, cover, 'Variable Cover')
+	conceal = name(Conceal, conceal, 'Variable Concealment')
+	maneuver = name(Maneuver, maneuver, 'Variable Maneuver')
+	weapon_melee = name(WeaponType, weapon_melee, 'Variable Melee Weapon')
+	weapon_ranged = name(WeaponType, weapon_ranged, 'Variable Ranged Weapon')
+	consequence = name(Consequence, consequence, 'Variable Consequence')
+	creature = name(Creature, creature, 'Variable Creature')
+	emotion = name(Emotion, emotion, 'Variable Emotion')
+	conflict = name(ConflictAction, conflict, 'Variable Conflict Action')
+	profession = name(Job, profession, 'Variable Profession')
+	bonus_check = name(Check, bonus_check,)
+	bonus_check_range = name(Ranged, bonus_check_range)
+	bonus_conflict = name(ConflictAction, bonus_conflict, 'Variable Conflict Action')
+	penalty_check = name(Check, penalty_check)
+	penalty_check_range = name(Ranged, penalty_check_range)
+	penalty_conflict = name(ConflictAction, penalty_conflict, 'Variable Conflict Action')
+	skill = name(Skill, skill)
+	light = name(Light, light)
+	effect = name(EquipEffect, effect)
+	feature = name(Feature, feature)
+
+	bonus_active_defense = variable_value('trait', bonus_effect, bonus_active_defense)
+	bonus_conflict_defend = variable_value('conflict', bonus_effect, bonus_conflict_defend)
+	penalty_active_defense = variable_value('trait', penalty_effect, penalty_active_defense)
+	penalty_conflict_defend = variable_value('conflict', penalty_effect, penalty_conflict_defend)
+
+	modifier_trigger = [{'type': '', 'name': 'Trigger'}, {'type': 'environment', 'name': 'Environment'}, {'type': 'cover', 'name': 'Cover'}, {'type': 'conceal', 'name': 'Concealment'}, {'type': 'sense', 'name': 'Sense'}, {'type': 'subsense', 'name': 'Subsense'}, {'type': 'condition', 'name': 'Condition'}, {'type': 'profession', 'name': 'Characters Profession'}, {'type': 'creature', 'name': 'Creature'}, {'type': 'power', 'name': 'Power'}, {'type': 'emotion', 'name': 'Emotion'}, {'type': 'consequence', 'name': 'Consequence'}, {'type': 'range', 'name': 'Range'}, {'type': 'critical', 'name': 'Critical Attempt'}, {'type': 'conflict', 'name': 'Conflict Action'}, {'type': 'maneuver', 'name': 'Maneuver'}, {'type': 'tools', 'name': 'Tool Requirement'}, {'type': 'ranged', 'name': 'Ranged Weapon'}, {'type': 'melee', 'name': 'Melee Weapon'}]
+
+	cells = cell('Feature', 15, [feature])
+	cells = cell('Effect', 15, [effect], cells)
+	cells = cell('Bonus', 12, [bonus], cells)
+	cells = cell('Type', 8, [bonus_type], cells)
+	vcells = vcell('effect', 15, ['Effect Modifier'])
+	vcells = vcell('attack', 12, ['Attack Bonus'], vcells)
+	vcells = vcell('damage', 13, ['Damage Bonus'], vcells)
+	vcells = vcell('defense', 16, ['Active Defenses'], vcells)
+	vcells = vcell('trait', 18, [bonus_trait], vcells)
+	word = string(', Range:', [bonus_check_range])
+	vcells = vcell('check', 29, [bonus_check, word, bonus_check_range], vcells)
+	vcells = vcell('conflict', 16, [bonus_conflict], vcells)
+	cells = vcell_add('Bonus Effect', bonus_effect, vcells, cells)
+
+	cells = check_cell('Active', 8, bonus_active_defense, cells)
+	cells = check_cell('Only Active', 11, bonus_conflict_defend, cells)
+	
+	cells = cell('Penalty', 12, [penalty], cells)
+	cells = cell('Type', 8, [penalty_type], cells)
+	
+	vcells = vcell('effect', 15, ['Effect Modifier'])
+	vcells = vcell('attack', 12, ['Attack Bonus'], vcells)
+	vcells = vcell('damage', 13, ['Damage Bonus'], vcells)
+	vcells = vcell('defense', 16, ['Active Defenses'], vcells)
+	vcells = vcell('trait', 18, [penalty_trait], vcells)
+	word = string(', Range:', [penalty_check_range])
+	vcells = vcell('check', 29, [penalty_check, word, penalty_check_range], vcells)
+	vcells = vcell('conflict', 16, [penalty_conflict], vcells)
+	
+	cells = vcell_add('Affects', penalty_effect, vcells, cells)
+
+	cells = check_cell('Active', 8, penalty_active_defense, cells)
+	cells = check_cell('Only Active', 11, penalty_conflict_defend, cells)
+	
+	vcells = vcell('environment', 20, [environment])
+	vcells = vcell('cover', 20, [cover], vcells)
+	vcells = vcell('conceal', 20, [conceal], vcells)
+	vcells = vcell('sense', 20, [sense], vcells)
+	vcells = vcell('subsense', 20, [subsense], vcells)
+	vcells = vcell('condition', 20, [condition], vcells)
+	vcells = vcell('profession', 20, [profession], vcells)
+	vcells = vcell('creature', 20, [creature], vcells)
+	vcells = vcell('power', 20, [power], vcells)
+	vcells = vcell('emotion', 20, [emotion], vcells)
+	vcells = vcell('consequence', 20, [consequence], vcells)
+	vcells = vcell('range', 20, [mod_range], vcells)
+	vcells = vcell('critical', 20, ['Critical Attempt'], vcells)
+	vcells = vcell('conflict', 20, [conflict], vcells)
+	vcells = vcell('maneuver', 20, [maneuver], vcells)
+	vcells = vcell('tools', 20, [tools], vcells)
+	vcells = vcell('ranged', 20, [weapon_ranged], vcells)
+	vcells = vcell('melee', 20, [weapon_melee], vcells)
+	vcells = vcell('skill', 20, [skill], vcells)
+	vcells = vcell('light', 20, [light], vcells)
+	cells = vcell_add('Trigger', trigger, vcells, cells)
+
+	body = send(cells, body)
+	
+	cells.clear()
+
+	return (body)
 
 
 
