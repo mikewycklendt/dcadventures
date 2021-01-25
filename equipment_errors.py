bb16f96eb37cc89ecfec783cdd1fee5d1677fe3d
Mike <mikewycklendt@gmail.com>
2021-01-25 14:13:49 -0600

    equipment

--- equipment_errors.py
+++ equipment_errors.py
@@ -38,7 +38,7 @@ def equip_save_errors(data):
 	errors = required(description, 'Description', errors)
 	errors = required(type_id, 'Equipment Type', errors)
 	errors = required(cost, 'Cost', errors)
-	if toughness != '6':
+	if type_id != '6':
 		errors = required(toughness, 'Toughness', errors)
 
 	errors = check_fields(move, 'Movement Effect', [speed_mod, direction], errors)
