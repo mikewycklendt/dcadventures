4609ec62bdfffeb204e811e1e6c33cdf06feec69
Mike <mikewycklendt@gmail.com>
2021-01-26 06:19:17 -0600

    equipment

--- static/js/weapon_create/base_form.js
+++ static/js/weapon_create/base_form.js
@@ -1,8 +1,10 @@
 function category() {
 	const select = 'category';
 	const fill = 'type';
+	const options = [{'val': '1', 'div': 'base-thrown'}]
 
 	weapon_type_select(select, fill);
+	select_opacity(select, options)
 }
 
 function power() {
