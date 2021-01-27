8f7ebd74fd37733eb9c528badaa9d74d83f60f39
Mike <mikewycklendt@gmail.com>
2021-01-27 09:32:39 -0600

    vehicle create

--- one_time/vehicle_size_create.py
+++ one_time/vehicle_size_create.py
@@ -1,44 +1,44 @@
 
-@app.route('/conceal/create')
-def conceal_create():
+@app.route('/vehiclesize/create')
+def vehiclesize_create():
 
 	entries = []
 
 	entries.append({'name': 'Awesome',
-					'size': ,
+					'size': 3,
 					'strength': 20,
 					'toughness': 15,
 					'defense': -10})
 
 	entries.append({'name': 'Colossal',
-					'size': ,
-					'strength': ,
-					'toughness': ,
-					'defense': })
+					'size': 2,
+					'strength': 16,
+					'toughness': 13,
+					'defense': -8})
 
-	entries.append({'name': '',
-					'size': ,
-					'strength': ,
-					'toughness': ,
-					'defense': })
+	entries.append({'name': 'Gargantuan',
+					'size': 1,
+					'strength': 12,
+					'toughness': 11,
+					'defense': -6})
 
-	entries.append({'name': '',
-					'size': ,
-					'strength': ,
-					'toughness': ,
-					'defense': })
+	entries.append({'name': 'Huge',
+					'size': 0,
+					'strength': 8,
+					'toughness': 9,
+					'defense': -4})
 
-	entries.append({'name': '',
-					'size': ,
-					'strength': ,
-					'toughness': ,
-					'defense': })
+	entries.append({'name': 'Large',
+					'size': -1,
+					'strength': 4,
+					'toughness': 7,
+					'defense': -2})
 
-	entries.append({'name': '',
-					'size': ,
-					'strength': ,
-					'toughness': ,
-					'defense': })
+	entries.append({'name': 'Medium',
+					'size': -2,
+					'strength': 0,
+					'toughness': 5,
+					'defense': 0})
 
 	for i in entries:
 		name = i['name']
@@ -61,8 +61,9 @@ def conceal_create():
 	for result in results:
 		print (result.id)
 		print (result.name)
+		print ('size: ' + str(result.size))
 		print ('strength: ' + str(result.strength))
 		print ('toughness: ' + str(result.toughness))
 		print ('defense: ' + str(result.defense))
 
-	return ('concealment added')
+	return ('vehicle sizes added')
