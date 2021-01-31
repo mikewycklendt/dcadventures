eb6df9e3d96fb8dd45a5057f8247ffaa59c93c99
Mike <mikewycklendt@gmail.com>
2021-01-31 10:44:46 -0600

    headquarters create

--- static/js/headquarters_create/info functions.js
+++ static/js/headquarters_create/info functions.js
@@ -54,7 +54,7 @@ function feature_info(field, route) {
 			const item = 'head-feature';
 			const divs = [{'val': jsonResponse.name, 'div': 'head-feature-name'},
 					{'val': jsonResponse.description, 'div': 'head-feature-description'},
-					{'val': jsonResponse.weapons, 'class': 'head-feature-item', 'icon': 'weapon-icon', 'div': 'head-feature-weapon'},
+					{'val': jsonResponse.weapons, 'class': 'head-feature-item', 'icon': 'weapon-icon', 'div': 'head-feature-weapons'},
 					{'val': jsonResponse.features, 'class': 'head-feature-item', 'icon': 'feature-icon', 'div': 'head-feature-features'},
 					{'val': jsonResponse.equipment, 'class': 'head-feature-item', 'icon': 'equipment-icon', 'div': 'head-feature-equipment'}]
 			const entry = 'addon-entry';
