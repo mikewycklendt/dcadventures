4e1907c9b911aa928b4fff5757e7fdecbb342823
Mike <mikewycklendt@gmail.com>
2021-02-03 07:59:27 -0600

    headquarters create

--- static/js/skill_create/base_form.js
+++ static/js/skill_create/base_form.js
@@ -4,3 +4,20 @@ function skill() {
 
 	icon_select(select, skill_icon_select, div, false, true, true);
 }
+
+function skill_dc_type() {
+	const select = 'skill_dc_type';
+	const options = [{'val': 'value', 'div': "skill-dc-value"}, {'val': 'mod', 'div': "skill-dc-mod"}]
+
+	select_opacity(select, options)
+}
+
+function skill_dc_table() {
+	const check = "dc_check";
+	const base = 'dc-base';
+	const entry = 'dc-entry';
+	const field = 'skill_dc_type';
+	const value = 'table';
+
+	select_entry(check, base, entry, field, value);
+}
