be1de97313a1d40f24fb2d6311808b3780487fac
Mike <mikewycklendt@gmail.com>
2021-02-04 13:13:24 -0600

    skill create

--- db/skill.py
+++ db/skill.py
@@ -20,6 +20,14 @@ database_path = db_path
 app.config["SQLALCHEMY_DATABASE_URI"] = database_path
 db = SQLAlchemy()
 
+def setup_db(app):
+	database_path = db_path
+	app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+	app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+	db.app = app
+	db.init_app(app)
+	db.create_all()
+	
 setup_db(app)
 migrate = Migrate(app, db)
 
