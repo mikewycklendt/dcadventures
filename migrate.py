694c426e1c6e4f5f4dce64c064e2f059df6e1ef7
Mike <mikewycklendt@gmail.com>
2021-02-04 15:03:25 -0600

    skill create

--- migrate.py
+++ migrate.py
@@ -20,8 +20,7 @@ db = SQLAlchemy(app)
 
 # Import database models with app context
 with app.app_context():
-  from models import *
-  from skill_models import *
+  from models, skill_models import *
 
 migrate = Migrate(app, db)
 
