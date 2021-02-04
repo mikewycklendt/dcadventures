5a3edf1f2cdbe81e044f9f2385a2f3be55051b26
Mike <mikewycklendt@gmail.com>
2021-02-04 13:39:08 -0600

    skill create

--- db/skill.py
+++ db/skill.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, Response, flash, redirect, ur
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from models import SkillBonus
+from .models import SkillBonus
 
 from dotenv import load_dotenv
 
