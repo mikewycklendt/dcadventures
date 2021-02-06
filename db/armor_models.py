71c135c20af6761d2a32a1a01c5b6780f192b5a4
Mike <mikewycklendt@gmail.com>
2021-02-06 15:16:52 -0600

    skill create

--- db/armor_models.py
+++ db/armor_models.py
@@ -26,38 +26,6 @@ setup_db(app)
 migrate = Migrate(app, db)
 
 
-class Armor(db.Model):
-	__tablename__ = 'armor'
-	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-	name = db.Column(db.String())
-	description = db.Column(db.String())
-	type_id = db.Column(db.Integer, db.ForeignKey('armor_type.id'))
-	cost = db.Column(db.Integer)
-	material = db.Column(db.Integer, db.ForeignKey('materials.id'))
-	toughness = db.Column(db.Integer)
-	active = db.Column(db.Integer)
-	subtle = db.Column(db.Boolean)
-	perception = db.Column(db.Integer)
-	impervious = db.Column(db.Boolean)
-	defense = db.Column(db.Boolean)
-	descriptor = db.Column(db.Boolean)
-
-	def format(self):
-		return {
-			'id': self.id,
-			'name': self.name,
-			'description': self.description,
-			'type_id': self.type_id,
-			'cost': self.cost,
-			'material': self.material,
-			'toughness': self.toughness,
-			'active': self.active,
-			'subtle': self.subtle,
-			'perception': self.perception,
-			'impervious': self.impervious,
-			'defense': self.defense,
-			'descriptor': self.descriptor
-		}
 
 class ArmorType(db.Model):
 	__tablename__ = 'armor_type'
