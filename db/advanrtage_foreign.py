c742c2b787ccb40e376f04b0ed6a259e50f1a33d
Mike <mikewycklendt@gmail.com>
2021-02-09 06:34:44 -0600

    skill create

--- db/advanrtage_foreign.py
+++ db/advanrtage_foreign.py
@@ -7,56 +7,37 @@ class AdvEffort(db.Model):
 	
 	
 class AdvMod(db.Model):
-	__tablename__ = 'advantage_modifiers'
-	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-	benefit = db.Column(db.Integer, db.ForeignKey('benefits.id'))
-	advantage_id = db.Column(db.Integer, db.ForeignKey('advantages.id'))
-	bonus = db.Column(db.Integer)
-	bonus_type = db.Column(db.String())
-	penalty = db.Column(db.Integer)
-	penalty_type = db.Column(db.String())
-	trigger = db.Column(db.String())
-	bonus_effect = db.Column(db.String())
-	penalty_effect = db.Column(db.String())
-	environment = db.Column(db.Integer)
-	environment_other = db.Column(db.String())
-	sense = db.Column(db.Integer)
-	mod_range = db.Column(db.Integer)
-	subsense = db.Column(db.Integer)
-	cover = db.Column(db.Integer)
-	conceal = db.Column(db.Integer)
-	maneuver = db.Column(db.Integer)
-	weapon_melee = db.Column(db.Integer)
-	weapon_ranged = db.Column(db.Integer)
-	tools = db.Column(db.String())
+	
+	environment = db.Column(db.Integer, db.ForeignKey('environments.id'))
+	
+	sense = db.Column(db.Integer, db.ForeignKey('senses.id'))
+	mod_range = db.Column(db.Integer, db.ForeignKey('ranged.id'))
+	subsense = db.Column(db.Integer, db.ForeignKey('sub_senses.id'))
+	cover = db.Column(db.Integer, db.ForeignKey('cover.id'))
+	conceal = db.Column(db.Integer, db.ForeignKey('concealment.id'))
+	maneuver = db.Column(db.Integer, db.ForeignKey('maneuvers.id'))
+	weapon_melee = db.Column(db.Integer, db.ForeignKey('weapon_type.id'))
+	weapon_ranged = db.Column(db.Integer, db.ForeignKey('weapon_type.id'))
+	
 	condition = db.Column(db.Integer, db.ForeignKey('conditions.id'))
 	power = db.Column(db.Integer, db.ForeignKey('powers.id'))
-	consequence = db.Column(db.Integer)
-	creature = db.Column(db.Integer)
-	creature_other = db.Column(db.String())
-	emotion = db.Column(db.Integer)
-	emotion_other = db.Column(db.String())
-	conflict = db.Column(db.Integer)
-	profession = db.Column(db.Integer)
-	profession_other = db.Column(db.String())
-	bonus_trait_type = db.Column(db.String())
-	bonus_trait = db.Column(db.Integer)
-	bonus_check = db.Column(db.Integer)
-	bonus_check_range = db.Column(db.Integer)
-	bonus_conflict = db.Column(db.Integer)
-	penalty_trait_type = db.Column(db.String())
-	penalty_trait = db.Column(db.Integer)
-	penalty_check = db.Column(db.Integer)
-	penalty_check_range = db.Column(db.Integer)
-	penalty_conflict = db.Column(db.Integer)
-	bonus_active_defense = db.Column(db.Boolean)
-	bonus_conflict_defend = db.Column(db.Boolean)
-	penalty_active_defense = db.Column(db.Boolean)
-	penalty_conflict_defend = db.Column(db.Boolean)
-	multiple = db.Column(db.String())
-	multiple_count = db.Column(db.Integer)
-	lasts = db.Column(db.Integer)
-
+	consequence = db.Column(db.Integer, db.ForeignKey('consequences.id'))
+	creature = db.Column(db.Integer, db.ForeignKey('creature.id'))
+	
+	emotion = db.Column(db.Integer, db.ForeignKey('emotions.id'))
+	
+	conflict = db.Column(db.Integer, db.ForeignKey('conflict_actions.id'))
+	profession = db.Column(db.Integer, db.ForeignKey('jobs.id'))
+	
+	
+	
+	
+	
+	bonus_conflict = db.Column(db.Integer, db.ForeignKey('conflict_sctions.id'))
+	
+	penalty_conflict = db.Column(db.Integer, db.ForeignKey('conflict_actions.id'))
+	
+	
 
 
 class AdvPoints(db.Model):
--- db/advanrtage_modeks.py
+++ db/advanrtage_modeks.py
@@ -532,37 +532,37 @@ class AdvMod(db.Model):
 	trigger = db.Column(db.String())
 	bonus_effect = db.Column(db.String())
 	penalty_effect = db.Column(db.String())
-	environment = db.Column(db.Integer)
+
 	environment_other = db.Column(db.String())
-	sense = db.Column(db.Integer)
-	mod_range = db.Column(db.Integer)
-	subsense = db.Column(db.Integer)
-	cover = db.Column(db.Integer)
-	conceal = db.Column(db.Integer)
-	maneuver = db.Column(db.Integer)
-	weapon_melee = db.Column(db.Integer)
-	weapon_ranged = db.Column(db.Integer)
+
+
+
+
+
+
+
+
 	tools = db.Column(db.String())
 	condition = db.Column(db.Integer, db.ForeignKey('conditions.id'))
 	power = db.Column(db.Integer, db.ForeignKey('powers.id'))
-	consequence = db.Column(db.Integer)
-	creature = db.Column(db.Integer)
+
+
 	creature_other = db.Column(db.String())
-	emotion = db.Column(db.Integer)
+
 	emotion_other = db.Column(db.String())
-	conflict = db.Column(db.Integer)
-	profession = db.Column(db.Integer)
+
+
 	profession_other = db.Column(db.String())
 	bonus_trait_type = db.Column(db.String())
 	bonus_trait = db.Column(db.Integer)
 	bonus_check = db.Column(db.Integer)
 	bonus_check_range = db.Column(db.Integer)
-	bonus_conflict = db.Column(db.Integer)
+
 	penalty_trait_type = db.Column(db.String())
 	penalty_trait = db.Column(db.Integer)
 	penalty_check = db.Column(db.Integer)
 	penalty_check_range = db.Column(db.Integer)
-	penalty_conflict = db.Column(db.Integer)
+
 	bonus_active_defense = db.Column(db.Boolean)
 	bonus_conflict_defend = db.Column(db.Boolean)
 	penalty_active_defense = db.Column(db.Boolean)
