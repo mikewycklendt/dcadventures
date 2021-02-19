7175ba1361afccadbf5df2f6e6f3d3aed5d83027
Mike <mikewycklendt@gmail.com>
2021-02-19 06:51:17 -0600

    skill rules create

--- skills.py
+++ skills.py
@@ -4,7 +4,7 @@ import dateutil.parser
 import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for, jsonify
 from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, literal_column
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
