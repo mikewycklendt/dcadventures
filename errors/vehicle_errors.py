f8278d0dcee6f162a7ad512986c1b05e843c5826
Mike <mikewycklendt@gmail.com>
2021-02-20 14:52:52 -0600

    skill rules create

--- errors/vehicle_errors.py
+++ errors/vehicle_errors.py
@@ -24,7 +24,6 @@ from functions.create_posts import one, field, int_word, select_multiple, string
 
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
-from error_functions import integer, required, power_check, one, field, rule_check, rule_select, cost_check, extra_cost, variable, select, variable_fields, variable_field, select_variable, together, check_together_var, together_names, check_fields, check_field, multiple, check_of_multiple, of_multiple, check_of, of, select_of, id_check, extra_check, extra_convert, int_check, db_integer, db_check, if_fields, if_field, create_check, db_insert, adv_entry_check, adv_check_multiple, adv_check_multiple_fields, adv_select_entry, name_exist, dependent, either, feature_check, equip_entry_check, equip_check_multiple_fields, required_multiple, weap_entry_check, arm_entry_check, no_zero
 
 
 def veh_save_errors(data):
