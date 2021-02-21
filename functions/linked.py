3cb170742770360409efe9780a803866b0e7b42b
Mike <mikewycklendt@gmail.com>
2021-02-21 05:01:49 -0600

    skill rules create

--- functions/linked.py
+++ functions/linked.py
@@ -79,6 +79,41 @@ def linked_options(table, trait, column):
 	options = []
 
 		
+	entries = db.session.query(table).all()
+	for e in entries:
+		if e.keyword is None:
+			keyword = ''
+		else:
+			keyword = e.keyword
+		id = getattr(e, column)
+		try:
+			entry_name = db.session.query(trait).filter(trait.id == id).one()
+			options.append({'id': e.id, 'name': str(e.id) +  entry_name.name + ' ' + keyword})
+		except:
+			print('hidden')
+
+	return (options)
+
+def linked_options_hide(table, trait, column):
+	options = []
+
+		
+	entries = db.session.query(table).filter(trait.hide == None).all()
+	for e in entries:
+		if e.keyword is None:
+			keyword = ''
+		else:
+			keyword = e.keyword
+		id = getattr(e, column)
+		entry_name = db.session.query(trait).filter(trait.id == id).one()
+		options.append({'id': e.id, 'name': str(e.id) +  entry_name.name + ' ' + keyword})
+
+	return (options)
+
+def linked_options(table, trait, column):
+	options = []
+
+		
 	entries = db.session.query(table).filter(trait.hide == None).all()
 	for e in entries:
 		if e.keyword is None:
