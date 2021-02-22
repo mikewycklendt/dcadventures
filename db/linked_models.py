c05d2cd3602a6003afab62a6417b7ce593244f86
Mike <mikewycklendt@gmail.com>
2021-02-22 08:25:37 -0600

    skill rules create

---
+++ db/linked_models.py
@@ -0,0 +1,92 @@
+from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+
+from models import setup_db
+from models import db
+
+load_dotenv()
+
+import os
+
+db_path = os.environ.get("db_path")
+
+app = Flask(__name__)
+moment = Moment(app)
+#app.config.from_object('config')
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_path = db_path
+app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+
+setup_db(app)
+
+
+migrate = Migrate(app, db)
+
+
+class SkillCircType(db.Model):
+	__tablename__ = 'skill_circ_type'
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	name = db.Column(db.String())
+	skill_id = db.Column(db.Integer, db.ForeignKey('skill_bonus.id'))
+
+	def format(self):
+		return {
+			'id': self.id,
+			'name': self.name,
+			'skill_id': self.skill_id
+		}
+		
+class SkillDCType(db.Model):
+	__tablename__ = 'skill_dc_type'
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	name = db.Column(db.String())
+	skill_id = db.Column(db.Integer, db.ForeignKey('skill_bonus.id'))
+
+	def format(self):
+		return {
+			'id': self.id,
+			'name': self.name,
+			'skill_id': self.skill_id
+		}
+		
+class SkillDegreeType(db.Model):
+	__tablename__ = 'skill_degree_type'
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	name = db.Column(db.String())
+	skill_id = db.Column(db.Integer, db.ForeignKey('skill_bonus.id'))
+
+	def format(self):
+		return {
+			'id': self.id,
+			'name': self.name,
+			'skill_id': self.skill_id
+		}
+
+class SkillMoveType(db.Model):
+	__tablename__ = 'skill_move_type'
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	name = db.Column(db.String())
+	skill_id = db.Column(db.Integer, db.ForeignKey('skill_bonus.id'))
+
+	def format(self):
+		return {
+			'id': self.id,
+			'name': self.name,
+			'skill_id': self.skill_id
+		}
+
+class SkillTimeType(db.Model):
+	__tablename__ = 'skill_time_type'
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	name = db.Column(db.String())
+	skill_id = db.Column(db.Integer, db.ForeignKey('skill_bonus.id'))
+
+	def format(self):
+		return {
+			'id': self.id,
+			'name': self.name,
+			'skill_id': self.skill_id
+		}
--- migrate.py
+++ migrate.py
@@ -24,6 +24,7 @@ with app.app_context():
   from db.rule_models import *
   from db.user_rules import *
   from db.measure_models import *
+  from db.linked_models import *
   from db.skill_models import *
   from db.advanrtage_modeks import *
   from db.vehicle_models import *
