1955adc0e7118d8f03ca0708664f5f1322a5671c
Mike <mikewycklendt@gmail.com>
2021-02-23 06:01:34 -0600

    skill rules create

--- base_files.py
+++ base_files.py
@@ -1,5 +1,5 @@
 
-stylesheets = [{"style": "/static/css/template/template.css"}, {"style": "/static/css/template/sidebar.css"}, {"style": "/static/css/icons.css"}]
+stylesheets = [{"style": "/static/css/template/template.css"}, {"style": "/static/css/template/sidebar.css"}, {"style": "/static/css/icons.css"}, {"style": "/static/fontawesome/css/all.css"}]
 meta_name="DC Adventures Online"
 meta_content="An online DC Comics Roleplaying game. Play as your favorite character or create your own hero."
 title = 'DC Adventures Online Roleplaying Game'
