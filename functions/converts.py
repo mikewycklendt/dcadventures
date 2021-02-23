065a156ce4e082993dc92fb655da2d5172c41903
Mike <mikewycklendt@gmail.com>
2021-02-23 06:07:04 -0600

    skill rules create

--- functions/converts.py
+++ functions/converts.py
@@ -443,7 +443,7 @@ def math_convert(name):
 		name =  ''
 		return (name)
 	elif name == 1:
-		name = '<i class="fas fa-plus">'
+		name = '<i class="fas fa-minus">'
 	elif name == 2:
 		name = '<i class="fas fa-minus">'
 	elif name == 4:
