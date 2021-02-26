77e9153e4ffa44c4aae0b7a319528c2c5a853946
Mike <mikewycklendt@gmail.com>
2021-02-26 11:17:30 -0600

    skill rules create

--- functions/linked.py
+++ functions/linked.py
@@ -95,7 +95,7 @@ def level_add(id, column, level, level_type, body):
 		add_title = False
  
 	try:
-		entry = db.session.query(Level).filter(Level.type_id == title_id, Level.name == level).first()
+		entry = db.session.query(Levels).filter(Levels.type_id == title_id, Level.name == level).first()
 		if entry is not None:
 			success = False
 			error_msgs.append('You have already created a rule with that keyword for this title.')
