62a374eb1b3e65a4760b2d81424bd533c2c1e74b
Mike <mikewycklendt@gmail.com>
2021-03-02 14:57:25 -0600

    skill rules create

--- posts/skill_posts.py
+++ posts/skill_posts.py
@@ -682,6 +682,8 @@ def skill_degree_post(entry, body, cells):
 	vcells = vcell('check', 35, [compare, 'Comparison Check'], vcells, 7, check_type)
 	vcells = vcell('check', 35, [variable, 'Variable Check'], vcells, variable_id, check_type)
 
+	vcells = vcell('object', 25, ['Object Destroyed'], vcells)
+
 	cells = vcell_add('Effect', type, vcells, cells)
 	
 	cells = cell('Nullify DC', 13, [nullify], cells)
