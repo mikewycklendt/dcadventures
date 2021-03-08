c3f6804e5aac04dadf0ebfee8c92bac5e3770ced
Mike <mikewycklendt@gmail.com>
2021-03-08 14:58:02 -0600

    skill rules create

--- app.py
+++ app.py
@@ -90,6 +90,90 @@ def shutdown_session(exception=None):
 
 
 
+@app.route('/close/create')
+def subskill_close_create():
+
+	ability = 5
+	skill = 3
+	check_type = 5
+	action = 1
+	attack = integer('skill')
+
+	styles = db.session.query(WeaponStyle).filter_by(type_id=1).all()
+
+	for i in styles:
+
+		if i.id == 14:
+			description = 'Attack Bonus equal to this rank for all unarmed attacks.'
+		else:
+			description = 'Attack Bonus equal to this rank for attacks with ' + i.name
+ 
+		entry = SkillBonus(name=i, show=True, base=True, subskill=True, description=description, weapon_style=i.id, ability=ability, skill=skill, check_type=check_type, action=action, attack=attack)
+		db.session.add(entry)
+		db.session.commit()
+
+	results = db.session.query(SkillBonus).filter_by(subskill=True)
+
+	for result in results:
+		print (result.id)
+		print (result.name)
+
+	return ('subskill added')
+
+@app.route('/close/create')
+def subskill_ranged_create():
+
+	entries = ['Unarmed']
+	ability = 4
+	skill = 11
+	check_type = 5
+	action = 1
+	attack = integer('skill')
+
+	styles = db.session.query(WeaponStyle).filter_by(type_id=2).all()
+
+	for i in styles:
+	
+		description = 'Attack Bonus equal to this rank for attacks with ' + i.name + ' Weapons.'
+ 
+		entry = SkillBonus(name=i, show=True, base=True, subskill=True, description=description, weapon_style=i.id, ability=ability, skill=skill, check_type=check_type, action=action, attack=attack)
+		db.session.add(entry)
+		db.session.commit()
+
+	results = db.session.query(SkillBonus).filter_by(subskill=True)
+
+	for result in results:
+		print (result.id)
+		print (result.name)
+
+	return ('subskill added')
+
+
+@app.route('/expertise/create')
+def subskill_expertise_create():
+
+	ability = db_integer(Ability, 'gm')
+	skill = 11
+	check_type = 3
+	action = 1
+
+	styles = db.session.query(Job).all()
+
+	for i in styles:
+	
+		description = 'Can answer all ' + i.name + ' related questions and perform ' + i.name + ' routine tasks as routine checks.'
+ 
+		entry = SkillBonus(name=i, show=True, base=True, subskill=True, description=description, profession=i.id, ability=ability, skill=skill, check_type=check_type, action=action, attack=attack)
+		db.session.add(entry)
+		db.session.commit()
+
+	results = db.session.query(SkillBonus).filter_by(subskill=True)
+
+	for result in results:
+		print (result.id)
+		print (result.name)
+
+	return ('subskill added')
 
 if __name__ == '__main__':
 	app.debug = True
--- one_time/subskills._create.py
+++ one_time/subskills._create.py
@@ -58,22 +58,21 @@ def subskill_ranged_create():
 	return ('subskill added')
 
 
-@app.route('/close/create')
-def subskill_ranged_create():
+@app.route('/expertise/create')
+def subskill_expertise_create():
 
-	ability = 4
+	ability = db_integer(Ability, 'gm')
 	skill = 11
 	check_type = 3
 	action = 1
-	attack = integer('skill')
 
 	styles = db.session.query(Job).all()
 
 	for i in styles:
 	
-		description = 'Can perform most ' + i.name + ' related tasks as routine checks.'
+		description = 'Can answer all ' + i.name + ' related questions and perform ' + i.name + ' routine tasks as routine checks.'
  
-		entry = SkillBonus(name=i, show=True, base=True, subskill=True, description=description, weapon_style=i.id, ability=ability, skill=skill, check_type=check_type, action=action, attack=attack)
+		entry = SkillBonus(name=i, show=True, base=True, subskill=True, description=description, profession=i.id, ability=ability, skill=skill, check_type=check_type, action=action, attack=attack)
 		db.session.add(entry)
 		db.session.commit()
 
