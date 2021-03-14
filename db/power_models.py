79c43ffa372a2e8db12e8d40c77e5a1978d8171f
Mike <mikewycklendt@gmail.com>
2021-03-14 15:55:07 -0500

    skill rules create

--- db/power_models.py
+++ db/power_models.py
@@ -1237,6 +1237,7 @@ class PowerChar(db.Model):
 	insub_type = db.Column(db.String())
 	insub_description = db.Column(db.String())
 	cost = db.Column(db.Integer, db.ForeignKey('power_cost.id'))	
+	ranks = db.Column(db.Integer, db.ForeignKey('power_ranks.id'))
 
 
 	def format(self):
@@ -1351,6 +1352,7 @@ class PowerCreate(db.Model):
 	support_effort = db.Column(db.Integer)
 	support_effort_rounds = db.Column(db.Integer)
 	cost = db.Column(db.Integer, db.ForeignKey('power_cost.id'))
+	ranks = db.Column(db.Integer, db.ForeignKey('power_ranks.id'))
 
 
 	def format(self):
@@ -1555,6 +1557,7 @@ class PowerEnv(db.Model):
 	visibility_trait = db.Column(db.Integer)
 	visibility_mod = db.Column(db.Integer)
 	cost = db.Column(db.Integer, db.ForeignKey('power_cost.id'))
+	ranks = db.Column(db.Integer, db.ForeignKey('power_ranks.id'))
 
 
 	def format(self):
@@ -1669,7 +1672,7 @@ class PowerMod(db.Model):
 	subtle = db.Column(db.Boolean)
 	permanent = db.Column(db.Boolean)
 	points = db.Column(db.Boolean)
-
+	ranks_check = db.Column(db.Boolean)
 	action = db.Column(db.Boolean)
 	side_effect = db.Column(db.Boolean)
 	concentration = db.Column(db.Boolean)
@@ -1731,7 +1734,7 @@ class PowerMod(db.Model):
 	points_reroll_cost = db.Column(db.Integer)
 	points_rerolls = db.Column(db.Integer)
 	points_reroll_result = db.Column(db.String())
-
+	ranks = db.Column(db.Integer, db.ForeignKey('power_ranks.id'))
 	cost = db.Column(db.Integer, db.ForeignKey('power_cost.id'))
 
 	def format(self):
@@ -2044,7 +2047,7 @@ class PowerSenseEffect(db.Model):
 	distance_unit = db.Column(db.Integer, db.ForeignKey('unit_type.id'))
 	distance_factor = db.Column(db.Integer)
 	dimensional_type = db.Column(db.String())
-
+	ranks = db.Column(db.Integer, db.ForeignKey('power_ranks.id'))
 	cost = db.Column(db.Integer, db.ForeignKey('power_cost.id'))
 
 
