1711e8efd27828f6c05011fab18d9ce6a03caabd
Mike <mikewycklendt@gmail.com>
2021-04-10 11:23:21 -0500

    skill rules create

--- base_files.py
+++ base_files.py
@@ -3,4 +3,21 @@ stylesheets = [{"style": "/static/css/template/global.css"}, {"style": "/static/
 meta_name="DC Adventures Online"
 meta_content="An online DC Comics Roleplaying game. Play as your favorite character or create your own hero."
 title = 'DC Adventures Online Roleplaying Game'
-sidebar = ["rules", "games", "stories", "heroes","npcs", "locations", "skills", "abilities", "powers", "flaws", "equipment", "devices", "armor", "weapons", "vehicles", "constructs", "help"]
+
+sidebar = [{'name': 'rules', 'link': '#'}, 
+			{'name': 'games', 'link': '#'}, 
+			{'name': 'stories', 'link': '#'}, 
+			{'name': 'heroes', 'link': '#'}, 
+			{'name': 'npcs', 'link': '#'}, 
+			{'name': 'locations', 'link': '#'}, 
+			{'name': 'skills', 'link': '/skill/create'}, 
+			{'name': 'abilities', 'link': '#'}, 
+			{'name': 'powers', 'link': '/power/create'}, 
+			{'name': 'flaws', 'link': '#'}, 
+			{'name': 'equipment', 'link': '/equipment/create'}, 
+			{'name': 'devices', 'link': '#'}, 
+			{'name': 'armor', 'link': '/armor/create'}, 
+			{'name': 'weapons', 'link': '/weapon/create'}, 
+			{'name': 'vehicles', 'link': '/vehicle/create'}, 
+			{'name': 'constructs', 'link': '#'}, 
+			{'name': 'help', 'link': '#'}]
