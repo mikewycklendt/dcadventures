c84c32f1c232291bd799080a820de138a396a999
Mike <mikewycklendt@gmail.com>
2021-04-15 10:46:47 -0500

    skill rules create

--- static/js/template.js
+++ static/js/template.js
@@ -1,3 +1,15 @@
+const modal = 'modal';
+
 function show_login() {
-	const modal = 'modal'
+	const div = 'login';
+
+	show_div(modal, 'flex');
+	show_div(div, 'block');
+}
+
+function login_close() {
+	const div = 'login';
+
+	hide_div(div, 200);
+	hide_div(modal, 200);
 }
