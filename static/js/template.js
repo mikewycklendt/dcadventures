d817df733c63dfecaf517c01845935bc0f02f99d
Mike <mikewycklendt@gmail.com>
2021-06-12 12:24:33 -0500

    skill rules create

--- static/js/template.js
+++ static/js/template.js
@@ -1,15 +1,13 @@
-const modal = 'modal';
 
 function show_login() {
 	const div = 'login';
 
-	show_div(modal, 'flex');
-	show_div(div, 'block');
+	show_modal(div);
 
 }
 
 function login_close() {
 	const div = 'login';
 
-	hide_div(div, 2);
+	hide_modal(div);
 }
